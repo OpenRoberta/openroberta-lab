d1286fadb9133b6693409927cf3249018467aa33
Daniel Pyka <daniel.pyka@iais.fraunhofer.de>
2014-04-01 11:42:42 +0200

    fix

--- EV3HelloWorld/src/EV3HelloWorld.java
+++ EV3HelloWorld/src/EV3HelloWorld.java
@@ -1,14 +1,12 @@
 import lejos.hardware.Button;
 import lejos.hardware.lcd.LCD;
-import lejos.utility.Delay;
 
-public class EV3HelloWorld
-{
-    public static void main(String[] args)
-    {
-    	LCD.drawString("Beispiel", 0, 2);
-    	LCD.drawString("Programm", 0, 3);
-    	Button.LEDPattern(4);
-    	Delay.msDelay(3000);
+public class EV3HelloWorld {
+    public static void main(String[] args) {
+        LCD.clear();
+        LCD.drawString("Hallo Welt", 0, 3);
+        Button.waitForAnyPress();
+        LCD.clear();
+        LCD.refresh();
     }
 }
--- EV3Menu/src/lejos/ev3/startup/RobertaUtils.java
+++ EV3Menu/src/lejos/ev3/startup/RobertaUtils.java
@@ -8,57 +8,44 @@ import java.io.ObjectInputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
-public class RobertaUtils
-{
+public class RobertaUtils {
 
-	public RobertaUtils()
-	{
-		//
-	}
-	
-	public void getProgram(URL serverURL, String programDir, String programName)
-	{
-		try
-		{
-			saveProgram(requestToServer(openConnection(serverURL)), programDir, programName);
-		}
-		catch (ClassNotFoundException | IOException e)
-		{
-			e.printStackTrace();
-		}
-	}
+    public RobertaUtils() {
+        //
+    }
 
-  private HttpURLConnection openConnection(URL url) throws IOException
-  {
-  	HttpURLConnection httpURLConnection = null;
-    httpURLConnection = (HttpURLConnection) url.openConnection();
-    httpURLConnection.setDoOutput(true);
-    httpURLConnection.setRequestMethod("GET");
-    return httpURLConnection;
-  }
-  
-  private byte[] requestToServer(HttpURLConnection httpURLConnection)
-  throws IOException, ClassNotFoundException
-  {
-    InputStream is = httpURLConnection.getInputStream();
-    ObjectInputStream ois = new ObjectInputStream(is);
-    byte[] ServletAnswer = (byte[]) ois.readObject();
-    ois.close();
-    is.close();
-    return ServletAnswer;
-  }
-  
-	private void saveProgram(byte[] program, String programDir, String programName) throws IOException
-	{
-		File userProgram = new File(programDir, programName);
-		FileOutputStream fis = new FileOutputStream(userProgram);
-		try
-		{
-			fis.write(program);
-		}
-		finally
-		{
-			fis.close();
-		}
-	}
+    public void getProgram(URL serverURL, String programDir, String programName) {
+        try {
+            saveProgram(requestToServer(openConnection(serverURL)), programDir, programName);
+        } catch ( ClassNotFoundException | IOException e ) {
+            e.printStackTrace();
+        }
+    }
+
+    private HttpURLConnection openConnection(URL url) throws IOException {
+        HttpURLConnection httpURLConnection = null;
+        httpURLConnection = (HttpURLConnection) url.openConnection();
+        httpURLConnection.setDoOutput(true);
+        httpURLConnection.setRequestMethod("GET");
+        return httpURLConnection;
+    }
+
+    private byte[] requestToServer(HttpURLConnection httpURLConnection) throws IOException, ClassNotFoundException {
+        InputStream is = httpURLConnection.getInputStream();
+        ObjectInputStream ois = new ObjectInputStream(is);
+        byte[] ServletAnswer = (byte[]) ois.readObject();
+        ois.close();
+        is.close();
+        return ServletAnswer;
+    }
+
+    private void saveProgram(byte[] program, String programDir, String programName) throws IOException {
+        File userProgram = new File(programDir, programName);
+        FileOutputStream fis = new FileOutputStream(userProgram);
+        try {
+            fis.write(program);
+        } finally {
+            fis.close();
+        }
+    }
 }
