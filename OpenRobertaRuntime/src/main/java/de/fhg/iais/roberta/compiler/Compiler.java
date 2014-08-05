5d5088406917f2efe582934c2d4c9fc72ace3fe5
Daniel Pyka <daniel.pyka@iais.fraunhofer.de>
2014-08-05 12:58:14 +0200

    ORA-142 preparation for hal library reference

--- OpenRobertaRuntime/src/main/java/de/fhg/iais/roberta/compiler/Compiler.java
+++ OpenRobertaRuntime/src/main/java/de/fhg/iais/roberta/compiler/Compiler.java
@@ -1,21 +1,22 @@
 package de.fhg.iais.roberta.compiler;
 
 import java.io.File;
-import java.util.Properties;
 
 import org.apache.tools.ant.Project;
 import org.apache.tools.ant.ProjectHelper;
 
 public class Compiler {
 
-    private static Properties p = new Properties();
-
     public static void main(String[] args) {
         runBuild("userProjects", "01234567", "Main");
     }
 
     /**
-     * three parameters to retrieve the correct directory structure for saved projects
+     * three parameters to retrieve the correct directory structure for saved projects<br>
+     * ant tasks:<br>
+     * clean build directory<br>
+     * compile .java file to .class<br>
+     * create runnable .jar file with META-INF project informations<br>
      * 
      * @param directory
      * @param token
@@ -33,7 +34,8 @@ public class Compiler {
         project.setProperty("output.dir", project.getProperty("input.dir") + "\\build");
         project.setProperty("lejos.home", "/home/root/lejos");
         project.setProperty("program.jar", project.getProperty("output.dir") + "\\" + programName + ".jar");
-        project.setProperty("lib.dir", "c:\\installations\\.m2\\repository\\de\\fhg\\iais\\roberta\\ev3classes\\0.8.1-beta");
+        project.setProperty("ev3classes.lib", "c:\\installations\\.m2\\repository\\de\\fhg\\iais\\roberta\\ev3classes\\0.8.1-beta");
+        project.setProperty("hal.lib", /* hal reference*/"");
         project.setProperty("java.home", "C:\\Program Files\\Java\\jdk1.7.0_51");
 
         project.init();
