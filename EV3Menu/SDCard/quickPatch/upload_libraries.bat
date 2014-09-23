56b448e0accb192ee558639739b791a57b1be844
Daniel Pyka <daniel.pyka@iais.fraunhofer.de>
2014-09-23 14:06:05 +0200

    small changes to upload batch script

---
+++ EV3Menu/SDCard/quickPatch/upload_libraries.bat
@@ -0,0 +1,2 @@
+pscp -scp -pw "" ..\..\..\OpenRobertaRuntime\target\OpenRobertaRuntime-0.0.1-SNAPSHOT.jar ..\..\..\OpenRobertaShared\target\OpenRobertaShared-0.0.1-SNAPSHOT.jar root@10.0.1.1:/home/roberta/lib
+PAUSE
---
+++ EV3Menu/SDCard/quickPatch/upload_menu.bat
@@ -0,0 +1,2 @@
+pscp -scp -pw "" ..\..\dist\EV3Menu.jar root@10.0.1.1:/home/root/lejos/bin/utils
+PAUSE
