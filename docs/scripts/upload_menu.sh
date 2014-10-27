5d23f8e0e00501fe9c484fd25dd44a20851fb33f
Daniel Pyka <daniel.pyka@iais.fraunhofer.de>
2014-10-27 12:27:43 +0100

    fix upload scripts from last commit

--- docs/scripts/upload_menu.sh
+++ docs/scripts/upload_menu.sh
@@ -1,6 +1,2 @@
-<<<<<<< HEAD
 scp ../../EV3Menu/dist/EV3Menu.jar root@10.0.1.1:/home/root/lejos/bin/utils
-=======
-scp ../../EV3Menu/dist/EV3Menu.jar root@10.116.20.147:/home/root/lejos/bin/utils
->>>>>>> ORA-219, ORA-138 refactoring ev3menu, option for canceling ip address input
 read -n1 -r -p "Press any key to continue..."
