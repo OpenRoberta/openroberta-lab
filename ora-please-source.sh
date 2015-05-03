928df68a6f773aaaec2cd31ca0b16310232d4954
Reinhard Budde <reinhard.budde@iais.fraunhofer.de>
2015-05-03 23:33:46 +0200

    fixed build problem. Check java 7 and javac on the PATH

--- ora-please-source.sh
+++ ora-please-source.sh
@@ -1,20 +1,7 @@
 #!/bin/bash
 
 ora.sh --java
-if [[ $? > 0 ]]; then
-  if [[ "$JAVA_HOME" != "" ]]; then
-    echo "trying to prefix your path with your JAVA_HOME to fix your path problem ..."
-    PATH="$JAVA_HOME/bin":$PATH
-    ora.sh --java
-    if [[ $? > 0 ]]; then
-      echo "... but this was NOT successful. You have to fix for path problem by yourself"
-    else
-      echo "... and this was successful"
-    fi
-  else
-    echo "JAVA_HOME is not set. You have to fix for path problem by yourself"
-  fi
-fi
+
 __commands_ora_sh=`ora.sh --help | tr [:blank:] '\n' | grep "^--"`
 
 function __complete_ora_sh {
