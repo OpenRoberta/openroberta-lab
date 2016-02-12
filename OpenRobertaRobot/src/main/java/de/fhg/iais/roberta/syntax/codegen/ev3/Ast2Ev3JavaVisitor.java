7ff10548a09addc04ccf54d01e2afe2e6ce05b58
Daniel Pyka <daniel.pyka91@gmail.com>
2016-02-12 12:43:25 +0100

    fix tests

--- OpenRobertaRobot/src/main/java/de/fhg/iais/roberta/syntax/codegen/ev3/Ast2Ev3JavaVisitor.java
+++ OpenRobertaRobot/src/main/java/de/fhg/iais/roberta/syntax/codegen/ev3/Ast2Ev3JavaVisitor.java
@@ -887,8 +887,8 @@ public class Ast2Ev3JavaVisitor implements AstVisitor<Void> {
         this.sb.append("\n\n").append(INDENT).append("public void run() throws Exception {\n");
         incrIndentation();
         // this is needed for testing
-        this.sb.append(INDENT).append(INDENT).append("hal.startServerLoggingThread();");
-        this.sb.append(INDENT).append(INDENT).append(INDENT).append("\nhal.startScreenLoggingThread();");
+        //this.sb.append(INDENT).append(INDENT).append("hal.startServerLoggingThread();");
+        //this.sb.append(INDENT).append(INDENT).append(INDENT).append("\nhal.startScreenLoggingThread();");
         return null;
     }
 
