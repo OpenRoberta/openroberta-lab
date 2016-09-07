e4058cb71bf184887635487f0725b7718f2f95c4
Kostadin Cvejoski <kostadin.cvejoski@iais.fraunhofer.de>
2016-09-07 15:50:19 +0200

    #199 add absolute paths to compiler resources

--- RobotArdu/src/main/java/de/fhg/iais/roberta/factory/ArduFactory.java
+++ RobotArdu/src/main/java/de/fhg/iais/roberta/factory/ArduFactory.java
@@ -55,7 +55,7 @@ public class ArduFactory extends AbstractRobotFactory {
                 Util1.getRobertaProperty("robot.plugin." + robotPropertyNumber + ".compiler.resources.dir"),
                 Util1.getRobertaProperty("robot.plugin." + robotPropertyNumber + ".compiler.dir"));
         this.robotId = robotId;
-        this.arduProperties = Util1.loadProperties("classpath:NXT.properties");
+        this.arduProperties = Util1.loadProperties("classpath:Ardu.properties");
     }
 
     @Override
