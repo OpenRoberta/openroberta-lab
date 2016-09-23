8bcd3f7c78958ad9d81ac11ab58fc315e4576064
Stefan Sauer <ensonic@users.sf.net>
2016-09-23 14:28:28 +0200

    Add missing new class.
    See OpenRoberta/robertalab-ev3dev#21

---
+++ OpenRobertaRobot/src/main/java/de/fhg/iais/roberta/components/UsedActor.java
@@ -0,0 +1,73 @@
+package de.fhg.iais.roberta.components;
+
+import de.fhg.iais.roberta.inter.mode.action.IActorPort;
+
+/**
+ * Stores information for port and mode of used actor in a blockly program.
+ * This information is used for program validation.
+ *
+ * @author ensonic
+ */
+
+public class UsedActor {
+    private final IActorPort port;
+    private final ActorType type;
+
+    public UsedActor(IActorPort port, ActorType type) {
+        this.port = port;
+        this.type = type;
+    }
+
+    /**
+     * @return the port
+     */
+    public IActorPort getPort() {
+        return this.port;
+    }
+
+    /**
+     * @return the actorType
+     */
+    public ActorType getType() {
+        return this.type;
+    }
+
+    @Override
+    public String toString() {
+        return "UsedActor [" + this.port + ", " + this.type + "]";
+    }
+
+    @Override
+    public final int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + this.port.hashCode();
+        result = prime * result + this.type.hashCode();
+        return result;
+    }
+
+    @Override
+    public final boolean equals(Object obj) {
+        if ( this == obj ) {
+            return true;
+        }
+        if ( obj == null ) {
+            return false;
+        }
+        if ( getClass() != obj.getClass() ) {
+            return false;
+        }
+        UsedActor other = (UsedActor) obj;
+        if ( this.type != other.type ) {
+            return false;
+        }
+        if ( this.port == null ) {
+            if ( other.port != null ) {
+                return false;
+            }
+        } else if ( this.port != other.port ) {
+            return false;
+        }
+        return true;
+    }
+}
