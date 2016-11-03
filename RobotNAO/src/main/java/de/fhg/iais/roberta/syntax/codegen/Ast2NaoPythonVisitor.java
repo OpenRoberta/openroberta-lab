2ff6bfe5c1264ca6be135425e45c8e02c7ad219e
JanisM91 <janis.mohr@hs-bochum.de>
2016-11-03 16:45:04 +0100

    minor change on the prefix for better readability

--- RobotNAO/src/main/java/de/fhg/iais/roberta/syntax/codegen/Ast2NaoPythonVisitor.java
+++ RobotNAO/src/main/java/de/fhg/iais/roberta/syntax/codegen/Ast2NaoPythonVisitor.java
@@ -1507,8 +1507,8 @@ public class Ast2NaoPythonVisitor implements AstVisitor<Void> {
 
         this.sb.append("from optparse import OptionParser\n\n");
         this.sb.append("from naoqi import ALBroker\n");
-        this.sb.append("from naoqi import ALProxy" + " #import ALProxy\n\n");
-        this.sb.append("import hal");
+        this.sb.append("from naoqi import ALProxy" + " #import ALProxy\n");
+        this.sb.append("import hal \n\n");
 
         this.sb.append("#parse command-line options\n");
         this.sb.append("parser = OptionParser()\n");
