86b676a4e9b2b7caea9f71812cbbac480f2ff4ef
Evgeniya Ovchinnikova <elhed@yandex.ru>
2017-02-01 16:31:12 +0100

    #373 added more tests for code generation (show text, move, etc.)

--- RobotArdu/src/test/java/de/fhg/iais/roberta/syntax/codegen/Ast2CVisitorTest.java
+++ RobotArdu/src/test/java/de/fhg/iais/roberta/syntax/codegen/Ast2CVisitorTest.java
@@ -3,6 +3,7 @@ package de.fhg.iais.roberta.syntax.codegen;
 import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Ignore;
+import org.junit.Test;
 
 import de.fhg.iais.roberta.components.Actor;
 import de.fhg.iais.roberta.components.ActorType;
@@ -18,16 +19,29 @@ import de.fhg.iais.roberta.testutil.Helper;
 public class Ast2CVisitorTest {
 
     private static final String MAIN_METHOD = ""
-        + "#define WHEELDIAMETER 5.6\n"
-        + "#define TRACKWIDTH 17.0\n"
-        + "#include\"hal.h\""
-        + "#include\"NXCDefs.h\""
-
-        + "task main(){"
-        + "    SetSensor(IN_1,SENSOR_TOUCH);\n"
-        // + "    SetSensor(IN_4, SENSOR_SOUND);\n"
-        //+ "    SetSensor(IN_3, SENSOR_LIGHT);\n"
-        + "    SetSensor(IN_2,SENSOR_LOWSPEED);\n" + "long timer1;\n" + "SetTimerValue(timer1);";
+        + "#include<math.h>\n"
+        + "#include<CountUpDownTimer.h>\n"
+        + "#include<BnrOneA.h>//Bot'nRollONEAlibrary"
+        + "#include<BnrRescue.h>//Bot'nRollCoSpaceRescueModulelibrary"
+        + "#include<RobertaFunctions.h>//OpenRobertalibrary"
+        + "#include<SPI.h>//SPIcommunicationlibraryrequiredbyBnrOne.cpp"
+        + "#include<Wire.h>//alibraryrequiredbyBnrRescue.cppfortheadditionalsonar"
+        + "BnrOneAone;"
+        + "BnrRescuebrm;"
+        + "RobertaFunctionsrob(one,brm);"
+        + "#defineSSPIN2"
+        + "#defineMODULE_ADDRESS0x2C"
+        + "bytecolorsLeft[3]={0,0,0};"
+        + "bytecolorsRight[3]={0,0,0};"
+        + "voidsetup(){"
+        + "Wire.begin();"
+        + "Serial.begin(9600);//setsbaudrateto9600bpsforprintingvaluesatserialmonitor."
+        + "one.spiConnect(SSPIN);//startstheSPIcommunicationmodule"
+        + "brm.i2cConnect(MODULE_ADDRESS);//startsI2Ccommunication"
+        + "brm.setModuleAddress(0x2C);"
+        + "one.stop();"
+        + "rob.setOne(one);"
+        + "rob.setBrm(brm);}";
 
     private static final String SUFFIX = "";
     private static ArduConfiguration brickConfiguration;
@@ -43,13 +57,14 @@ public class Ast2CVisitorTest {
         brickConfiguration = (ArduConfiguration) builder.build();
     }
 
-    @Ignore
+    @Test
     public void test() throws Exception {
 
         final String a = "" //
             //            + CONSTANTS
             + MAIN_METHOD
-            + "        TextOut(0,LCD_LINE3,\"Hallo\");\n"
+            + "voidloop(){"
+            + "        one.lcd1(\"Hallo\");\n"
             + SUFFIX
 
             + "}\n";
@@ -57,77 +72,21 @@ public class Ast2CVisitorTest {
         assertCodeIsOk(a, "/syntax/code_generator/java/java_code_generator.xml");
     }
 
-    @Ignore
+    @Test
     public void test1() throws Exception {
 
-        final String a = ""
-            + MAIN_METHOD
+        final String a =
+            ""
+                + MAIN_METHOD
+                + "voidloop(){"
+                + "        for ( float k0 = 0; k0 < 10; k0+=1 ) {\n"
+                + "           one.lcd1(\"Hallo\");"
+                + "        }\n"
+                + SUFFIX
 
-            + "        for ( float k0 = 0; k0 < 10; k0+=1 ) {\n"
-            + "           TextOut(0,LCD_LINE3,\"Hallo\");"
-            + "        }\n"
-            + SUFFIX
-
-            + "}\n";
+                + "}\n";
 
         assertCodeIsOk(a, "/syntax/code_generator/java/java_code_generator1.xml");
-    }
-
-    //ignore
-    public void test2() throws Exception {
-
-        final String a = "" //
-            + MAIN_METHOD
-
-            + "        if (SENSOR_1) {\n"
-            + "          SENSOR_TYPE_LIGHT_ACTIVE;SetSensorLight(IN_3,IN_TYPE_COLORGREEN);\n"
-            + "        } else if ( Pickcolor.RED == Sensor(IN_)IN_TYPE_COLORCOLOUR);) {\n"
-            + "        \n"
-            + "            while ( true ) {\n"
-            + "               GraphicOut( 0, 0,\"EYESOPEN\");\n\n"
-            + "                  RotateMotor(OUT_B,30);"
-            + "            \n"
-            + "        }\n"
-            + "        }\n"
-            + "        playFile(1);\n"
-            + "        setVolume(50);\n"
-            + "        for ( float i = 1; i < 10; i += 1 ) {\n\n"
-            + "        RotateMotor(OUT_B,30,360.0*1);"
-            + "        }\n"
-            + SUFFIX
-
-            + "}\n";
-
-        assertCodeIsOk(a, "/syntax/code_generator/java/java_code_generator2.xml");
-    }
-
-    @Ignore
-    public void test3() throws Exception {
-
-        final String a = "" //
-
-            + MAIN_METHOD
-
-            + "        if (SENSOR_1) {\n"
-            + "           SENSOR_TYPE_LIGHT_ACTIVE;SetSensorLight(IN_3,IN_TYPE_COLORGREEN);\n"
-            + "        } else {\n"
-            + "            if (,\"1pressed\") {\n"
-            + "                SENSOR_TYPE_LIGHT_ACTIVE;SetSensorLight(IN_3,IN_TYPE_COLORGREEN);\n"
-            + "            } else if (0==SetSensorLowspeed(IN_4);) {\n"
-            + "               GraphicOut( 15, 15,\"FLOWERS\");\n"
-            + "            } else {\n"
-            + "            \n"
-            + "                while ( !hal.isPressed(BrickKey.UP) ) {\n\n"
-            + "                    RotateMotor(OUT_B,30);"
-            + "               \n"
-            + "            }\n"
-            + "            }\n"
-            + "        }\n"
-            + SUFFIX
-
-            + "}\n";
-
-        assertCodeIsOk(a, "/syntax/code_generator/java/java_code_generator3.xml");
     }
 
     @Ignore
@@ -136,7 +95,7 @@ public class Ast2CVisitorTest {
         final String a = "" //
 
             + MAIN_METHOD
-
+            + "voidloop(){"
             + "        if ( 5 < MotorPower(OUT_B); ) {\n\n\n"
             + "             RotateMotor(OUT_B,30);\n"
             + "          RotateMotor(OUT_B,30,360.0*1);\n"
@@ -161,17 +120,16 @@ public class Ast2CVisitorTest {
         assertCodeIsOk(a, "/syntax/code_generator/java/java_code_generator4.xml");
     }
 
-    @Ignore
+    @Test
     public void test5() throws Exception {
 
         final String a = "" //
             + MAIN_METHOD
-
-            + "        OnFwdReg(OUT_B,0,100);"
-            + "        RotateMotorEx(OUT_B,-30,360.0*0,-100,true,true);"
-            + "       OnFwdSync(OUT_AB,0,100);"
-            + "        SetVolume(50);"
-            + "        PlayTone(0,0);"
+            + "voidloop(){"
+            + "       one.move1m(1, 0);"
+            + "       rob.move1mTime(1,30,0);"
+            + "       one.movePID(0,-0);"
+            + "       tone(9,0,0);"
             + SUFFIX
 
             + "}\n";
@@ -179,14 +137,14 @@ public class Ast2CVisitorTest {
         assertCodeIsOk(a, "/syntax/code_generator/java/java_code_generator5.xml");
     }
 
-    @Ignore
+    @Test
     public void test6() throws Exception {
 
         final String a = "" //
             + MAIN_METHOD
-
-            + "        TextOut(0,LCD_LINE0,\"Hallo\");\n"
-            + "        PlayTone(300, 3000);\n"
+            + "voidloop(){"
+            + "        one.lcd1(\"Hallo\");\n"
+            + "        tone(9,300, 3000);\n"
             + SUFFIX
 
             + "}\n";
@@ -194,13 +152,13 @@ public class Ast2CVisitorTest {
         assertCodeIsOk(a, "/syntax/code_generator/java/java_code_generator6.xml");
     }
 
-    @Ignore
+    @Test
     public void test7() throws Exception {
         final String a = "" //
             + MAIN_METHOD
-
-            + "          OnFwdReg(OUT_B,30,100);\n"
-            + "          RotateMotorEx(OUT_B,-30,360.0*1,-100,true,true);\n"
+            + "voidloop(){"
+            + "          one.move1m(1,30);\n"
+            + "          rob.move1mTime(1,30,1);\n"
             + SUFFIX
 
             + "}\n";
@@ -208,17 +166,18 @@ public class Ast2CVisitorTest {
         assertCodeIsOk(a, "/syntax/code_generator/java/java_code_generator7.xml");
     }
 
-    @Ignore
+    @Test
     public void test8() throws Exception {
 
         final String a = "" //
             + MAIN_METHOD
-            + "        float item = 10;\n"
-            + "        string item2 = \"TTTT\";\n"
+            + "        double item = 10;\n"
+            + "        String item2 = \"TTTT\";\n"
             + "        bool item3 = true;\n"
-            + "        TextOut(0,LCD_LINE0,\"Hallo\");\n"
-            + "        TextOut(0,LCD_LINE0,\"Hallo\");\n"
-            + "        TextOut(0,LCD_LINE0,\"Hallo\");\n"
+            + "voidloop(){"
+            + "        one.lcd1(item);\n"
+            + "        one.lcd1(item2.c_str());\n"
+            + "        one.lcd1(rob.boolToString(item3));\n"
             + "        item3 = false;\n"
             + SUFFIX
 
@@ -227,176 +186,88 @@ public class Ast2CVisitorTest {
         assertCodeIsOk(a, "/syntax/code_generator/java/java_code_generator8.xml");
     }
 
-    @Ignore
-    public void test19() throws Exception {
-
-        final String a = "" //
-            + MAIN_METHOD
-            + "        float variablenName = 0;\n"
-
-            + "OnFwdReg(OUT_AB,50);"
-            + "GraphicOut(0,0,\"OLDGLASSES\");"
-            + SUFFIX
-
-            + "}\n";
-
-        assertCodeIsOk(a, "/syntax/code_generator/java/java_code_generator9.xml");
-    }
-
-    //Test accepts some types, those don't exist in NXC. Should be fixed later, when the
-    //OpenRoberta for NXT is ready
-
-    // @Test
-    //public void test9() throws Exception {
-
-    //String a = "" //
-    //+ MAIN_METHOD
-    //+ "        floatitem=0;"
-    //+ "        stringitem2=\"ss\";"
-    //+ "        booleanitem3=true;"
-    //+ "        floatitem4={1,2,3};"
-    //+ "        stringitem5={\"a\",\"b\"};"
-    //+ "        booleanitem6={true,false};"
-
-    //+ SUFFIX
-
-    //+ "}\n";
-
-    //assertCodeIsOk(a, "/ast/task/task_mainTask.xml");
-    //}
-
-    @Ignore
-    public void test10() throws Exception {
-
-        final String a = "" //
-            + MAIN_METHOD
-
-            + "       RotateMotor(OUT_B, 30, 360 * 1);"
-            + "        macheEtwas(10, 10);"
-
-            + "   void macheEtwas(float x, float x2) {\n"
-            + "        GraphicOut(x, x2,\"OLDGLASSES\");\n"
-            + "    }"
-            + "}\n";
-
-        assertCodeIsOk(a, "/syntax/methods/method_void_1.xml");
-    }
-
-    @Ignore
+    @Test
     public void test11() throws Exception {
 
         final String a = "" //
             + MAIN_METHOD
-
-            + "        test();"
-
-            + "    void test() {\n"
-            + "    }"
+            + "void test() {\n"
+            + "    one.led(HIGH);"
+            + "}"
+            + "voidloop(){"
+            + "    test();"
             + "}\n";
 
         assertCodeIsOk(a, "/syntax/methods/method_void_2.xml");
     }
 
-    @Ignore
+    @Test
     public void test12() throws Exception {
 
         final String a = "" //
             + MAIN_METHOD
-
-            + "        test(true);"
-
-            + "     void test(bool x) {\n"
-            + "        if (x) return;"
-            + "    }"
+            + "void test(bool x) {\n"
+            + "    if (x) return;"
+            + "    one.led(HIGH);"
+            + "}"
+            + "void loop(){"
+            + "test(true);"
             + "}\n";
 
         assertCodeIsOk(a, "/syntax/methods/method_if_return_1.xml");
     }
 
-    @Ignore
+    @Test
     public void test13() throws Exception {
 
         final String a = "" //
             + MAIN_METHOD
-            + "    float variablenName=0;\n"
+            + "    double variablenName=0;\n"
             + "    bool variablenName2=true;\n"
-
-            + "        test1(0, 0);"
-            + "        test2();"
-
-            + "     void test1(float x, float x2) {\n"
-            + "        TextOut(x,LCD_LINEx2,\"Hallo\");\n"
+            + "     void test1(double x, double x2) {\n"
+            + "        one.lcd1(\"Hallo\");\n"
             + "    }\n\n"
             + "    void test2() {\n"
             + "        if (variablenName2) return;"
+            + "        one.led(HIGH);"
             + "    }"
+
+            + "void loop(){"
+            + "        test1(0, 0);"
+            + "        test2();"
             + "}\n";
 
         assertCodeIsOk(a, "/syntax/methods/method_void_3.xml");
     }
 
+    //TODO: fix the test
     @Ignore
     public void test14() throws Exception {
 
         final String a = "" //
             + MAIN_METHOD
-            + "    string variablenName[]={\"a\",\"b\",\"c\"};\n"
-
-            + "        TextOut(0,LCD_LINE0,"
-
-            + "     float test(float x, string x2[]) {\n"
-            + "       TextOut(x,LCD_LINE0,string(test(0,variablenName);\n"
+            + "String variablenNameRaw[3]={\"a\",\"b\",\"c\"};\n"
+            + "String *variablenName = variablenNameRaw;"
+            + "     double test(double x, Stringx2Raw[0]) {\n"
+            + "       one.lcd1(x2);\n"
             + "        return x;\n"
             + "    }"
+            + "void loop(){"
+            + "one.lcd1(test(0,variablenName));"
             + "}\n";
 
         assertCodeIsOk(a, "/syntax/methods/method_return_1.xml");
     }
 
-    //Test accepts some types, those don't exist in NXC. Should be fixed later, when the
-    //OpenRoberta for NXT is ready
-    //@Test
-    //public void test15() throws Exception {
-
-    //String a = "" //
-    //+ MAIN_METHOD
-    //+ "    string variablenName=BlocklyMethods.createListWithString(\"a\", \"b\", \"c\");\n"
-
-    //+ "        hal.drawText(String.valueOf(test()), 0, 0);"
-
-    //+ "    private Pickcolor test() {\n"
-    //+ "        hal.drawText(String.valueOf(variablenName), 0, 0);\n"
-    //+ "        return Pickcolor.NONE;\n"
-    //+ "    }"
-    //+"}\n";
-
-    //    assertCodeIsOk(a, "/syntax/methods/method_return_2.xml");
-    //}
-
-    //Test accepts some types, those don't exist in NXC. Should be fixed later, when the
-    //OpenRoberta for NXT is ready
-    //@Test
-    //public void test16() throws Exception {
-
-    //    String a = "" //
-
-    //        + "    string variablenName=BlocklyMethods.createListWithString(\"a\", \"b\", \"c\");\n"
-
-    //        + "        hal.drawText(String.valueOf(test()), 0, 0);"
-    //        + "}\n";
-
-    //    assertCodeIsOk(a, "/syntax/methods/method_if_return_2.xml");
-    //}
-
-    @Ignore
+    @Test
     public void test17() throws Exception {
         // regression test for https://mp-devel.iais.fraunhofer.de/jira/browse/ORA-610
         final String a = "" //
             + MAIN_METHOD
-            + "    string message=\"exit\";\n"
-
-            + "        if (message.equals(\"exit\")) {\n"
-            + "           TextOut(0,LCD_LINE0,\"done\");"
+            + "    String message=\"exit\";\n"
+            + "voidloop(){"
+            + "        if (message == \"exit\") {\n"
+            + "           one.lcd1(\"done\");"
             + "        }\n"
 
             + "}\n";
@@ -404,13 +275,13 @@ public class Ast2CVisitorTest {
         assertCodeIsOk(a, "/syntax/stmt/if_stmt4.xml");
     }
 
-    @Ignore
+    @Test
     public void test18() throws Exception {
         final String a = "" //
             + MAIN_METHOD
-            + "    float item=0;\n"
-            + "    string item2=\"cc\";\n"
-
+            + "    double item=0;\n"
+            + "    String item2=\"cc\";\n"
+            + "voidloop(){"
             + "}\n";
 
         assertCodeIsOk(a, "/syntax/code_generator/java/java_code_generator11.xml");
