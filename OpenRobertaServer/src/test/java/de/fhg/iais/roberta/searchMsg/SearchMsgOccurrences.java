cc5381efcb0cff4546f09629dd7a8e70ec320475
Reinhard Budde <reinhard.budde@iais.fraunhofer.de>
2017-03-18 20:08:03 +0100

    #447 SearchMsgOccurrences 0.9

--- OpenRobertaServer/src/test/java/de/fhg/iais/roberta/searchMsg/SearchMsgOccurrences.java
+++ OpenRobertaServer/src/test/java/de/fhg/iais/roberta/searchMsg/SearchMsgOccurrences.java
@@ -6,36 +6,86 @@ import java.io.IOException;
 import java.io.LineNumberReader;
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.HashSet;
+import java.util.Collections;
 import java.util.List;
-import java.util.Set;
+import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import org.junit.Assert;
-import org.junit.Ignore;
-import org.junit.Test;
 
 import com.google.common.collect.ArrayListMultimap;
 import com.google.common.collect.ListMultimap;
 
+import de.fhg.iais.roberta.util.Pair;
+
+/**
+ * This class<br>
+ * - gets in its constructor a file with keys (e.g. a property file, a JSON file with key-value pairs, ...)<br>
+ * - and using a pattern and further extraction logic it extracts all the keys from the file<br>
+ * - then it gets many pairs of directories and patterns<br>
+ * - for each pair it retrieves all files from the directory (recursively) matching the pattern<br>
+ * - for each file it iterates over all lines and checks whether one of the keys is used in the file<br>
+ * - it remembers the key found, the matching line and its line number in a multi map<br>
+ * - furthermore it remembers that the key has been found at all<br>
+ * - it prints a summary about the results for the whole directory<br>
+ * <br>
+ * - if all directories have been processed, it tells which keys are <b>not</b> used.<br>
+ * - the matching lines can be printed, too (has to be activated, because the output is huge)<br>
+ * <br>
+ * For different use,<br>
+ * - adapt the extraction logic in {@link SearchMsgOccurrences#getMsgList(File)}<br>
+ * - adapt the print logic<br>
+ * - and adapt the list of directories to be searched (see class {@link SearchMsgOccurrencesTest}
+ *
+ * @author lbudde + rbudde
+ */
 public class SearchMsgOccurrences {
+    private static final boolean DEBUG = true; // true: debugging into to syso; false: only infos and errors
+    private final List<String> msgList;
+    private final ListMultimap<String, MsgHits> msgKeyOccurrences = ArrayListMultimap.create();
+    private final List<String> msgNotYetMatched;
+
+    public SearchMsgOccurrences(File msgKeyFile) throws IOException {
+        this.msgList = getMsgList(msgKeyFile);
+        Collections.sort(this.msgList);
+        this.msgNotYetMatched = new ArrayList<>(this.msgList);
+        String propfilePath = msgKeyFile.getCanonicalPath();
+        System.out.println("Found " + this.msgList.size() + " message keys in file " + propfilePath);
+        if ( false && DEBUG ) {
+            sysoList(this.msgList);
+        }
+    }
+
+    public void search(File file, Pattern fileNamePattern) throws Exception {
+        List<File> files = filterDir(file, fileNamePattern);
+        String filePath = file.getCanonicalPath();
+        Pair<Integer, Integer> mC = findTheMsg(files);
+        System.out.println("In " + filePath + " with " + files.size() + " files: " + mC.getFirst() + " keys and " + mC.getSecond() + " keys first time");
+    }
+
+    public void statistics() {
+        System.out.println("The following message keys seem to be unused:");
+        sysoList(msgNotYetMatched);
+        System.out.println(this.msgList.size() + " message keys");
+        System.out.println(this.msgNotYetMatched.size() + " unused message keys");
+        // prettyPrint();
+    }
 
     /**
-     * Searches in a Property File for all the Strings which match the given Pattern.
-     * Then creates a List with all these Strings.
+     * searches a message key file for all Strings which represents message keys. Then creates a List with all these message keys.<br>
+     * <br>
+     * <b>For each use this method has to be changed to reflect different patterns and extraction of substrings.</b>
      *
-     * @param propfile Property File to work with
-     * @param pattern
-     * @return A List of the Strings which match the given Pattern.
-     * @throws IOException
+     * @param msgKeyFile File with message keys
+     * @return A List of the message keys
      */
-    public List<String> getMsgList(File propfile, String pattern) throws IOException {
-        Pattern regex = Pattern.compile(pattern);
-        Assert.assertNotNull(propfile);
+    private List<String> getMsgList(File msgKeyFile) throws IOException {
+        Pattern regex = Pattern.compile("^Blockly.Msg.[^ ]+");
+        Assert.assertNotNull(msgKeyFile);
         String line;
-        List<String> msgList = new ArrayList<String>();
-        try (LineNumberReader lineReader = new LineNumberReader(new FileReader(propfile))) {
+        List<String> msgs = new ArrayList<String>();
+        try (LineNumberReader lineReader = new LineNumberReader(new FileReader(msgKeyFile))) {
             while ( (line = lineReader.readLine()) != null ) {
                 Matcher matcher = regex.matcher(line);
                 if ( matcher.find() ) {
@@ -44,26 +94,55 @@ public class SearchMsgOccurrences {
                     if ( group.startsWith("ORA_") ) {
                         group = group.substring(4);
                     }
-                    msgList.add(group);
+                    msgs.add(group);
                 }
             }
         }
-        return msgList;
-
+        return msgs;
     }
 
     /**
-     * Searches in all Files for Strings which match the given patterns.
-     * Then creates a MultiMap, which maps the patterns to its occurrences.
+     * Seeks for all the non-Directory-Files in a given File(root). Then checks which of these file names match the given Pattern. The matching Files are then
+     * added to the resultDataList.
+     *
+     * @param root the File to work with
+     * @param pattern the pattern to look up in file names
+     * @return a List of Files, whose file names match the given Pattern
+     * @throws IOException
+     */
+    private List<File> filterDir(File root, Pattern pattern) throws IOException {
+        List<File> resultDataList = new ArrayList<>();
+
+        if ( root.isDirectory() ) {
+            File[] directoryFiles = root.listFiles();
+            if ( directoryFiles == null ) {
+                System.out.println("Directory is not readable and thus ignored: " + root.getCanonicalPath());
+                return resultDataList;
+            }
+            for ( File file : directoryFiles ) {
+                List<File> resultFiles = filterDir(file, pattern);
+                resultDataList.addAll(resultFiles);
+            }
+        } else {
+            Matcher matcher = pattern.matcher(root.getAbsolutePath());
+            if ( matcher.matches() ) {
+                resultDataList.add(root);
+            }
+        }
+        return resultDataList;
+    }
+
+    /**
+     * Searches in all Files for Strings which contain at least one of the messsage keys. Store matches in a MultiMap, which maps the keys to its occurrences.
      *
      * @param fileList to search within
      * @param regexList the List of patterns for searching
      * @return A MultiMap which maps the patterns to its occurrences.
      * @throws IOException
      */
-    public ListMultimap<String, MsgHits> findTheMsg(List<File> fileList, List<String> regexList) throws IOException, SecurityException {
-        ListMultimap<String, MsgHits> msgMultimap = ArrayListMultimap.create();
-        Set<String> unusedMsg = new HashSet<>(regexList);
+    private Pair<Integer, Integer> findTheMsg(List<File> fileList) throws Exception {
+        int msgKeysMatched = 0;
+        int msgKeysMatchedForTheFirstTime = 0;
         for ( File javaFile : fileList ) {
             int number;
             String line;
@@ -73,76 +152,38 @@ public class SearchMsgOccurrences {
             try (LineNumberReader lineReader = new LineNumberReader(new FileReader(javaFile))) {
 
                 while ( (line = lineReader.readLine()) != null ) {
-                    for ( String msg : regexList ) {
+                    for ( String msg : this.msgList ) {
                         if ( line.contains(msg) ) {
-                            unusedMsg.remove(msg);
+                            msgKeysMatched++;
+                            if ( this.msgNotYetMatched.remove(msg) ) {
+                                msgKeysMatchedForTheFirstTime++;
+                            }
                             number = lineReader.getLineNumber();
                             MsgHits msgChar = new MsgHits(javaFile.getCanonicalPath(), number, line);
-                            msgMultimap.put(msg, msgChar);
+                            msgKeyOccurrences.put(msg, msgChar);
                         }
                     }
 
                 }
             }
         }
-        System.out.println(unusedMsg);
-        return msgMultimap;
+        return Pair.of(msgKeysMatched, msgKeysMatchedForTheFirstTime);
     }
 
-    /**
-     * Seeks for all the non-Directory-Files in a given File(root).
-     * Then checks which of these Filenames match the given Pattern.
-     * The matching Files are then added to a List(resultDataList).
-     *
-     * @param root the File to work with
-     * @param pattern the pattern to look up in Filenames
-     * @return a List of Files, which Filenames match the given Pattern
-     */
-    public List<File> filterDir(File root, Pattern pattern) {
-
-        List<File> resultDataList = new ArrayList<>();
-
-        if ( root.isDirectory() ) {
-            File[] directoryFiles = root.listFiles();
-            if ( directoryFiles == null ) {
-                System.out.println("Directory is not readable: " + root.getPath());
-                return resultDataList;
-            }
-            for ( File file : directoryFiles ) {
-                List<File> resultFiles = filterDir(file, pattern);
-                resultDataList.addAll(resultFiles);
-            }
-        } else {
-
-            Matcher matcher = pattern.matcher(root.getAbsolutePath());
-            if ( matcher.matches() ) {
-                resultDataList.add(root);
-            }
-        }
-        return resultDataList;
-    }
-
-    private void prettyPrint(ListMultimap<String, MsgHits> result) {
-        for ( String msg : result.asMap().keySet() ) {
-            Collection<MsgHits> hits = result.asMap().get(msg);
+    private void prettyPrint() {
+        Map<String, Collection<MsgHits>> asMap = this.msgKeyOccurrences.asMap();
+        for ( String msg : asMap.keySet() ) {
+            Collection<MsgHits> hits = asMap.get(msg);
+            System.out.println(msg);
             for ( MsgHits hit : hits ) {
-                System.out.println(msg + ";" + hit.getFileName() + ";" + hit.getLineNumber() + ";" + hit.getContent());
+                System.out.println("  " + hit.getFileName() + ";" + hit.getLineNumber() + ";" + hit.getContent());
             }
         }
-
     }
 
-    @Ignore
-    @Test
-    public void test1() throws IOException {
-        Pattern pattern = Pattern.compile(".*");
-        File propfile = new File("../../blockly/robMsg/robMessages.js");
-        File origin = new File("./src/main/java");
-
-        List<File> fileList = filterDir(origin, pattern);
-        List<String> msgList = getMsgList(propfile, "^Blockly.Msg.[^ ]+");
-        ListMultimap<String, MsgHits> result = findTheMsg(fileList, msgList);
-        //prettyPrint(result);
+    private void sysoList(List<String> msgList) {
+        for ( String msg : msgList ) {
+            System.out.println("  " + msg);
+        }
     }
-
 }
---
+++ OpenRobertaServer/src/test/java/de/fhg/iais/roberta/searchMsg/SearchMsgOccurrencesTest.java
@@ -0,0 +1,38 @@
+package de.fhg.iais.roberta.searchMsg;
+
+import java.io.File;
+import java.util.regex.Pattern;
+
+import org.junit.Test;
+
+public class SearchMsgOccurrencesTest {
+    private static final Pattern ALL = Pattern.compile(".*");
+
+    @Test
+    public void testMessageOccurences() throws Exception {
+        SearchMsgOccurrences smo = new SearchMsgOccurrences(new File("../../blockly/robMsg/robMessages.js"));
+        smo.search(new File("../OpenRobertaRobot/src/main/java"), ALL);
+        smo.search(new File("../OpenRobertaServer/src/main/java"), ALL);
+        smo.search(new File("../RobotArdu/src/main/java"), ALL);
+        smo.search(new File("../RobotEV3/src/main/java"), ALL);
+        smo.search(new File("../RobotMbed/src/main/java"), ALL);
+        smo.search(new File("../RobotNAO/src/main/java"), ALL);
+        smo.search(new File("../RobotNXT/src/main/java"), ALL);
+
+        smo.search(new File("../OpenRobertaServer/staticResources/index.html"), ALL);
+        smo.search(new File("../OpenRobertaServer/staticResources/js"), ALL);
+
+        smo.search(new File("../../blockly/appengine"), ALL);
+        smo.search(new File("../../blockly/blocks"), ALL);
+        smo.search(new File("../../blockly/core"), ALL);
+        smo.search(new File("../../blockly/demos"), ALL);
+        smo.search(new File("../../blockly/generators"), ALL);
+        smo.search(new File("../../blockly/i18n"), ALL);
+        smo.search(new File("../../blockly/media"), ALL);
+        // smo.search(new File("../../blockly/msg"), ALL);
+        smo.search(new File("../../blockly/tests"), ALL);
+
+        smo.statistics();
+    }
+
+}
