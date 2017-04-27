2099208458881d95ffb9420305824f65a350cdab
Kostadin <cvejoski@gmail.com>
2017-04-27 14:47:14 +0200

    fix unit test

--- RobotNXT/src/test/java/de/fhg/iais/roberta/ast/syntax/expr/ExprTest.java
+++ RobotNXT/src/test/java/de/fhg/iais/roberta/ast/syntax/expr/ExprTest.java
@@ -16,11 +16,7 @@ public class ExprTest {
     @Test
     public void test2() throws Exception {
         final String a =
-<<<<<<< ce5e36408b7f6f9cacc93041b87e296db1504c33
             "\n2 * ( 2 - 2 )\n" + "2 - (2 * 2)(88 - ( 8 + (-3 + 5))) - (2 * 2)((88 - ( 8 + (-3 + 5))) - (2 * 2) )/ ((88 -( 8 + (-3 + 5)))-(2 * 2))";
-=======
-            "\n2 * ( 2 - 2 )\n" + "2 - (2 * 2)(88 - ( 8 + (-3 + 5))) - (2 * 2)((88 - ( 8 + (-3 + 5))) - (2 * 2) )/  ((88 -( 8 + (-3 + 5)))-(2 * 2) )";
->>>>>>> #474 remove casting to float NXC
 
         Helper.assertCodeIsOk(a, "/syntax/expr/expr2.xml");
     }
