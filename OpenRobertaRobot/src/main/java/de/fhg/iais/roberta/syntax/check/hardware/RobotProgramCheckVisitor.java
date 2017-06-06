9a6154a94fb27852a6d61dd3abcd86fb1620990e
Kostadin <cvejoski@gmail.com>
2017-06-06 07:43:23 +0200

    #514 add missing check for the show text block

--- OpenRobertaRobot/src/main/java/de/fhg/iais/roberta/syntax/check/hardware/RobotProgramCheckVisitor.java
+++ OpenRobertaRobot/src/main/java/de/fhg/iais/roberta/syntax/check/hardware/RobotProgramCheckVisitor.java
@@ -11,6 +11,7 @@ import de.fhg.iais.roberta.syntax.action.light.LightStatusAction;
 import de.fhg.iais.roberta.syntax.action.sound.PlayFileAction;
 import de.fhg.iais.roberta.syntax.action.sound.ToneAction;
 import de.fhg.iais.roberta.syntax.action.sound.VolumeAction;
+import de.fhg.iais.roberta.syntax.action.sound.VolumeAction.Mode;
 import de.fhg.iais.roberta.syntax.sensor.BaseSensor;
 import de.fhg.iais.roberta.typecheck.NepoInfo;
 
@@ -89,6 +90,9 @@ public abstract class RobotProgramCheckVisitor extends ProgramCheckVisitor {
 
     @Override
     public Void visitShowTextAction(ShowTextAction<Void> showTextAction) {
+        showTextAction.getMsg().visit(this);
+        showTextAction.getX().visit(this);
+        showTextAction.getY().visit(this);
         return null;
     }
 
@@ -104,11 +108,16 @@ public abstract class RobotProgramCheckVisitor extends ProgramCheckVisitor {
 
     @Override
     public Void visitToneAction(ToneAction<Void> toneAction) {
+        toneAction.getDuration().visit(this);
+        toneAction.getFrequency().visit(this);
         return null;
     }
 
     @Override
     public Void visitVolumeAction(VolumeAction<Void> volumeAction) {
+        if ( volumeAction.getMode() == Mode.SET ) {
+            volumeAction.getVolume().visit(this);
+        }
         return null;
     }
 
