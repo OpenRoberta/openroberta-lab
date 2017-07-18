c56b8899f31554a3b38af46aa4cdbcffd8180bc7
Evgeniya Ovchinnikova <elhed@yandex.ru>
2017-07-18 10:14:15 +0200

    #572 added program wrapper to the test so the resulting code would resemble the actually generated one

--- RobotNXT/src/test/java/de/fhg/iais/roberta/ast/syntax/actors/ToneActionFunctionTest.java
+++ RobotNXT/src/test/java/de/fhg/iais/roberta/ast/syntax/actors/ToneActionFunctionTest.java
@@ -10,14 +10,16 @@ public class ToneActionFunctionTest {
     @Test
     public void playTone() throws Exception {
         final String a =
-            "bytevolume=0x02;\n"
-                + "floatElement=0;\n"
-                + "taskmain(){macheEtwas();"
-                + "voidmacheEtwas(){\n"
+            "#defineWHEELDIAMETER0.0#defineTRACKWIDTH0.0#defineMAXLINES8#include\"NEPODefs.h\"//containsNEPOdeclarationsfortheNXCNXTAPIresources"
+                + "void macheEtwas();"
+                + "byte volume = 0x02;\n"
+                + "float Element=0;"
+                + "taskmain(){ macheEtwas();}"
+                + "void macheEtwas(){\n"
                 + "volume=50*4/100.0+0.5;"
                 + "PlayToneEx(300, 100, volume, false);\n"
                 + "Wait(100);}";
 
-        this.h.assertCodeIsOk(a, "/ast/actions/action_PlaySoundFunc.xml");
+        this.h.assertWrappedCodeIsOk(a, "/ast/actions/action_PlaySoundFunc.xml");
     }
 }
