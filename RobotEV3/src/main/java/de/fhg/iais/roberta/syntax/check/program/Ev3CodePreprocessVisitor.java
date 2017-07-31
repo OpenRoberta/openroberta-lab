6168e1400aac383b80411de80c96ed692999b151
Kostadin <cvejoski@gmail.com>
2017-07-31 12:51:16 +0200

    #108 create asset storage for static pictures

--- RobotEV3/src/main/java/de/fhg/iais/roberta/syntax/check/program/Ev3CodePreprocessVisitor.java
+++ RobotEV3/src/main/java/de/fhg/iais/roberta/syntax/check/program/Ev3CodePreprocessVisitor.java
@@ -1,10 +1,13 @@
 package de.fhg.iais.roberta.syntax.check.program;
 
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
 
 import de.fhg.iais.roberta.components.Configuration;
 import de.fhg.iais.roberta.mode.sensor.ev3.GyroSensorMode;
 import de.fhg.iais.roberta.syntax.Phrase;
+import de.fhg.iais.roberta.syntax.action.display.ShowPictureAction;
 import de.fhg.iais.roberta.syntax.sensor.generic.GyroSensor;
 import de.fhg.iais.roberta.syntax.sensor.generic.TemperatureSensor;
 
@@ -14,10 +17,15 @@ import de.fhg.iais.roberta.syntax.sensor.generic.TemperatureSensor;
  * @author kcvejoski
  */
 public class Ev3CodePreprocessVisitor extends PreprocessProgramVisitor {
+    private final Set<String> usedImages = new HashSet<String>();
 
     public Ev3CodePreprocessVisitor(ArrayList<ArrayList<Phrase<Void>>> phrasesSet, Configuration brickConfiguration) {
         super(brickConfiguration);
         check(phrasesSet);
+    }
+
+    public Set<String> getUsedImages() {
+        return this.usedImages;
     }
 
     @Override
@@ -30,6 +38,13 @@ public class Ev3CodePreprocessVisitor extends PreprocessProgramVisitor {
 
     @Override
     public Void visitTemperatureSensor(TemperatureSensor<Void> temperatureSensor) {
+        return null;
+    }
+
+    @Override
+    public Void visitShowPictureAction(ShowPictureAction<Void> showPictureAction) {
+        super.visitShowPictureAction(showPictureAction);
+        this.usedImages.add(showPictureAction.getPicture().toString());
         return null;
     }
 
