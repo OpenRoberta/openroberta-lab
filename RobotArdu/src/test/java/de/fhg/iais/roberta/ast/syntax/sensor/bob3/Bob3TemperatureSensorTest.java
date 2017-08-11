85864f33dae2085ac98b64f55e66ea659af54256
Artem Vinokurov <artem.vinokurov@inf.h-brs.de>
2017-08-11 11:29:39 +0200

    #345 added temperature sensor test for bob3

---
+++ RobotArdu/src/test/java/de/fhg/iais/roberta/ast/syntax/sensor/bob3/Bob3TemperatureSensorTest.java
@@ -0,0 +1,17 @@
+package de.fhg.iais.roberta.ast.syntax.sensor.bob3;
+
+import org.junit.Test;
+
+import de.fhg.iais.roberta.util.test.ardu.HelperBob3;
+
+public class Bob3TemperatureSensorTest {
+
+    HelperBob3 h = new HelperBob3();
+
+    @Test
+    public void getLeftArmRightArmLight() throws Exception {
+        final String a = "double item; void setup(){ Serial.begin(9600); item = 0; } void loop() { item = myBob.getTemperature();";
+
+        this.h.assertCodeIsOk(a, "/ast/sensors/sensor_bob3Temperature.xml", false);
+    }
+}
