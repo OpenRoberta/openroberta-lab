7f83569775f375e1b593028d20247b1bd6c64033
Beate Jost <beate.jost@iais.fraunhofer.de>
2017-10-06 19:38:35 +0200

    #648 paste text as plain text in info view

--- OpenRobertaServer/staticResources/js/app/roberta/controller/progInfo.controller.js
+++ OpenRobertaServer/staticResources/js/app/roberta/controller/progInfo.controller.js
@@ -37,6 +37,21 @@ define([ 'exports', 'comm', 'message', 'log', 'util', 'guiState.controller', 'bl
                 });
             }
         });
+        // prevent to copy eg ms word formatting!
+        $('[contenteditable]').on('paste', function(e) {
+            e.preventDefault();
+            var text = '';
+            if (e.clipboardData || e.originalEvent.clipboardData) {
+              text = (e.originalEvent || e).clipboardData.getData('text/plain');
+            } else if (window.clipboardData) {
+              text = window.clipboardData.getData('Text');
+            }
+            if (document.queryCommandSupported('insertText')) {
+              document.execCommand('insertText', false, text);
+            } else {
+              document.execCommand('paste', false, text);
+            }
+        });
     }
 
     function toggleInfo() {
