ad6f00d3d0e675a7f04201c42a9ff1e5a3736315
Philipp Maurer <Philipp.Maurer@iais.fraunhofer.de>
2017-11-22 13:03:39 +0100

    Set the key test to @Ignore again

--- OpenRobertaParent/OpenRobertaServer/src/test/java/de/fhg/iais/roberta/searchMsg/SearchMsgOccurrencesTest.java
+++ OpenRobertaParent/OpenRobertaServer/src/test/java/de/fhg/iais/roberta/searchMsg/SearchMsgOccurrencesTest.java
@@ -4,12 +4,11 @@ import java.io.File;
 import java.util.regex.Pattern;
 
 import org.junit.Ignore;
-import org.junit.Test;
 
 public class SearchMsgOccurrencesTest {
     private static final Pattern ALL = Pattern.compile(".+");
 
-    @Test
+    @Ignore
     public void testMessageOccurences() throws Exception {
         SearchMsgKeyOccurrences smo =
             new SearchMsgKeyOccurrences(new File("../../../blockly/robMsg/robMessages.js"), new File("../../../blockly/msg/messages.js"));
