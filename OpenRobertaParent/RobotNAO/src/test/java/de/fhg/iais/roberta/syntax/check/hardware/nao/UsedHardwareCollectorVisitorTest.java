ea8c7b869e6da66a00ab025a61e1fdbd2ef32830
Kostadin <cvejoski@gmail.com>
2018-04-09 12:33:12 +0200

    #142 add unit tests for NAO

--- OpenRobertaParent/RobotNAO/src/test/java/de/fhg/iais/roberta/syntax/check/hardware/nao/UsedHardwareCollectorVisitorTest.java
+++ OpenRobertaParent/RobotNAO/src/test/java/de/fhg/iais/roberta/syntax/check/hardware/nao/UsedHardwareCollectorVisitorTest.java
@@ -37,4 +37,84 @@ public class UsedHardwareCollectorVisitorTest {
 
     }
 
+    @Test
+    public void testGetNaoMarkInfo_returnsListWithOneUsedSensor() throws Exception {
+        ArrayList<ArrayList<Phrase<Void>>> phrases = this.h.generateASTs("/hardwarecheck/getnaomarkinfo.xml");
+
+        UsedHardwareCollectorVisitor checkVisitor = new UsedHardwareCollectorVisitor(phrases, makeConfiguration());
+
+        Assert.assertEquals("[UsedSensor [null, NAOMARK, null]]", checkVisitor.getUsedSensors().toString());
+    }
+
+    @Test
+    public void testDetectNaoMark_returnsListWithOneUsedSensor() throws Exception {
+        ArrayList<ArrayList<Phrase<Void>>> phrases = this.h.generateASTs("/hardwarecheck/detectnaomark.xml");
+
+        UsedHardwareCollectorVisitor checkVisitor = new UsedHardwareCollectorVisitor(phrases, makeConfiguration());
+
+        Assert.assertEquals("[UsedSensor [null, NAOMARK, null]]", checkVisitor.getUsedSensors().toString());
+    }
+
+    @Test
+    public void testGetRecogniezdWordFromList_returnsListWithOneUsedSensor() throws Exception {
+        ArrayList<ArrayList<Phrase<Void>>> phrases = this.h.generateASTs("/hardwarecheck/getrecognizedwordfromlist.xml");
+
+        UsedHardwareCollectorVisitor checkVisitor = new UsedHardwareCollectorVisitor(phrases, makeConfiguration());
+
+        Assert.assertEquals("[UsedSensor [null, NAOSPEECH, null]]", checkVisitor.getUsedSensors().toString());
+    }
+
+    @Test
+    public void testAllMoveBlocks_returnsListWithOneUsedSensor() throws Exception {
+        ArrayList<ArrayList<Phrase<Void>>> phrases = this.h.generateASTs("/hardwarecheck/moveblocks.xml");
+
+        UsedHardwareCollectorVisitor checkVisitor = new UsedHardwareCollectorVisitor(phrases, makeConfiguration());
+
+        Assert.assertEquals("[UsedSensor [null, ULTRASONIC, null]]", checkVisitor.getUsedSensors().toString());
+    }
+
+    @Test
+    public void testWalkDistance_returnsListWithOneUsedSensor() throws Exception {
+        ArrayList<ArrayList<Phrase<Void>>> phrases = this.h.generateASTs("/hardwarecheck/walkdistance.xml");
+
+        UsedHardwareCollectorVisitor checkVisitor = new UsedHardwareCollectorVisitor(phrases, makeConfiguration());
+
+        Assert.assertEquals("[UsedSensor [null, ULTRASONIC, null]]", checkVisitor.getUsedSensors().toString());
+    }
+
+    @Test
+    public void testTurnAction_returnsListWithOneUsedSensor() throws Exception {
+        ArrayList<ArrayList<Phrase<Void>>> phrases = this.h.generateASTs("/hardwarecheck/turnaction.xml");
+
+        UsedHardwareCollectorVisitor checkVisitor = new UsedHardwareCollectorVisitor(phrases, makeConfiguration());
+
+        Assert.assertEquals("[UsedSensor [null, ULTRASONIC, null]]", checkVisitor.getUsedSensors().toString());
+    }
+
+    @Test
+    public void testWalkToX_returnsListWithOneUsedSensor() throws Exception {
+        ArrayList<ArrayList<Phrase<Void>>> phrases = this.h.generateASTs("/hardwarecheck/walktoXYTheta1.xml");
+
+        UsedHardwareCollectorVisitor checkVisitor = new UsedHardwareCollectorVisitor(phrases, makeConfiguration());
+
+        Assert.assertEquals("[UsedSensor [null, ULTRASONIC, null]]", checkVisitor.getUsedSensors().toString());
+    }
+
+    @Test
+    public void testWalkToY_returnsListWithOneUsedSensor() throws Exception {
+        ArrayList<ArrayList<Phrase<Void>>> phrases = this.h.generateASTs("/hardwarecheck/walktoXYTheta2.xml");
+
+        UsedHardwareCollectorVisitor checkVisitor = new UsedHardwareCollectorVisitor(phrases, makeConfiguration());
+
+        Assert.assertEquals("[UsedSensor [null, ULTRASONIC, null]]", checkVisitor.getUsedSensors().toString());
+    }
+
+    @Test
+    public void testWalkToTheta_returnsListWithOneUsedSensor() throws Exception {
+        ArrayList<ArrayList<Phrase<Void>>> phrases = this.h.generateASTs("/hardwarecheck/walktoXYTheta3.xml");
+
+        UsedHardwareCollectorVisitor checkVisitor = new UsedHardwareCollectorVisitor(phrases, makeConfiguration());
+
+        Assert.assertEquals("[UsedSensor [null, ULTRASONIC, null]]", checkVisitor.getUsedSensors().toString());
+    }
 }
