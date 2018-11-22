772701432f8b0dec4fbc5d7cfc9771b6304131c6
Artem Vinokurov <artem.vinokurov@iais.fraunhofer.de>
2018-11-22 15:28:48 +0100

    added docker files for different base images, to run integration tests (no maven cache kept)

---
+++ Docker/integration/README.md
@@ -0,0 +1,3 @@
+ **These are Dockerfiles for running integration tests in docker with containers built off different base images**
+ 
+_...just run build-container.sh and provide the directory name with a docker file (without a trailing slash)_
---
+++ Docker/integration/build-container.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+if [ $# -eq 0 ]
+  then
+    echo "Please provide the directory with a Dockerfile."
+    exit 0
+fi
+
+if [ ! -d "$1" ]; then
+  echo $1 "is not present."
+  exit 0
+fi
+
+docker build -t robertalab/integration:v0.1-$1 $1
