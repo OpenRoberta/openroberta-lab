833d7431d044e4ae97788121667edc78234e5fda
Artem Vinokurov <artem.vinokurov@iais.fraunhofer.de>
2018-12-07 16:47:25 +0100

    issue #1071 - mbot now uses decimal in ColorConst + added a test

--- OpenRobertaParent/RobotArdu/src/main/java/de/fhg/iais/roberta/visitor/codegen/MbotCppVisitor.java
+++ OpenRobertaParent/RobotArdu/src/main/java/de/fhg/iais/roberta/visitor/codegen/MbotCppVisitor.java
@@ -138,15 +138,15 @@ public final class MbotCppVisitor extends AbstractCommonArduinoCppVisitor implem
         if ( lightAction.getRgbLedColor().getClass().equals(ColorConst.class) ) {
             String hexValue = ((ColorConst<Void>) lightAction.getRgbLedColor()).getRgbValue();
             hexValue = hexValue.split("#")[1];
+            int R = Integer.decode("0x" + hexValue.substring(0, 2));
+            int G = Integer.decode("0x" + hexValue.substring(2, 4));
+            int B = Integer.decode("0x" + hexValue.substring(4, 6));
             this.sb.append(", ");
-            this.sb.append("0x");
-            this.sb.append(hexValue.substring(0, 2));
+            this.sb.append(R);
             this.sb.append(", ");
-            this.sb.append("0x");
-            this.sb.append(hexValue.substring(2, 4));
+            this.sb.append(G);
             this.sb.append(", ");
-            this.sb.append("0x");
-            this.sb.append(hexValue.substring(4, 6));
+            this.sb.append(B);
             this.sb.append(");");
             nlIndent();
             this.sb.append("_meRgbLed.show();");
