dee2a37a3fb0226fa005950a4460fa7fbe33e55a
Reinhard Budde <reinhard.budde@iais.fraunhofer.de>
2019-01-21 11:30:39 +0100

    genLab.sh: pull must be before checkout

--- Docker/genLab.sh
+++ Docker/genLab.sh
@@ -28,8 +28,8 @@ then
     BUILD_ALL='true'
 fi
 echo "building branch $BRANCH with version $VERSION. Build all container is set to $BUILD_ALL"
-git checkout $BRANCH
 git pull
+git checkout $BRANCH
 cd /opt/robertalab/OpenRobertaParent
 mvn clean install
 chmod +x RobotArdu/resources/linux/arduino-builder RobotArdu/resources/linux/tools-builder/ctags/5.8*/ctags
