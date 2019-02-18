d1c16240c5bf570fe4da420d496fe6c24a787c9f
Reinhard Budde <reinhard.budde@iais.fraunhofer.de>
2019-02-18 17:33:02 +0100

    'botnroll' updater more robust

--- OpenRobertaParent/OpenRobertaServer/src/main/java/de/fhg/iais/roberta/persistence/util/Upgrader_3_1_0.java
+++ OpenRobertaParent/OpenRobertaServer/src/main/java/de/fhg/iais/roberta/persistence/util/Upgrader_3_1_0.java
@@ -1,5 +1,7 @@
 package de.fhg.iais.roberta.persistence.util;
 
+import java.math.BigInteger;
+
 import org.hibernate.Session;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -26,10 +28,26 @@ public class Upgrader_3_1_0 {
      */
     public void run() {
         this.nativeSession = sessionFactoryWrapper.getNativeSession();
-        new DbSetup(nativeSession)
+        DbExecutor dbExecutor = DbExecutor.make(nativeSession);
+        if ( ((BigInteger) dbExecutor.oneValueSelect("select count(*) from ROBOT where NAME = 'botnroll'")).longValue() == 0 ) {
+            LOG.info("inserting 'botnroll' into the ROBOT table");
+            dbExecutor.update("insert into ROBOT values (DEFAULT,'botnroll',now,null,0)");
+        } else {
+            LOG.info("'botnroll' found in the ROBOT table");
+        }
+        if ( ((BigInteger) dbExecutor.oneValueSelect("select count(*) from ROBOT where NAME = 'ardu'")).longValue() == 0 ) {
+            LOG.info("inserting 'ardu' into the ROBOT table");
+            dbExecutor.update("insert into ROBOT values (DEFAULT,'ardu',now,null,0)");
+        } else {
+            LOG.info("'ardu' found in the ROBOT table");
+        }
+        dbExecutor.ddl("commit");
+
+        DbSetup dbSetup = new DbSetup(this.nativeSession);
+        dbSetup
             .sqlFile(
-                "select count(*) from ROBOT where NAME = 'botnroll'", //
-                "select count(*) from ROBOT where NAME = 'botnroll'", //
+                null, //
+                null,
                 "/update-3-1-0.sql");
         nativeSession.createSQLQuery("shutdown").executeUpdate();
         nativeSession.close();
