37d6fe4ad9a898b23f37d58254e2229fe03970e7
Artem Vinokurov <artem.vinokurov@iais.fraunhofer.de>
2019-03-04 15:10:33 +0100

    issue #1138 - added sdcard write test and OLED screen off test - added missing OLED screen off block to the toolboxes

--- OpenRobertaParent/RobotArdu/src/test/java/de/fhg/iais/roberta/syntax/codegen/sensebox/SenseboxActorTest.java
+++ OpenRobertaParent/RobotArdu/src/test/java/de/fhg/iais/roberta/syntax/codegen/sensebox/SenseboxActorTest.java
@@ -10,6 +10,17 @@ public class SenseboxActorTest {
     private final HelperSenseboxForXmlTest senseboxHelper = new HelperSenseboxForXmlTest();
 
     @Test
+    public void sdCardOledOffTest() throws Exception {
+        Configuration config =
+            this.senseboxHelper.regenerateConfiguration(Util1.readResourceContent("/ast/actions/sensebox_write_sdcard_display_clear_test_config.xml"));
+        this.senseboxHelper
+            .compareExistingAndGeneratedSource(
+                "/ast/actions/sensebox_write_sdcard_display_clear_test.ino",
+                "/ast/actions/sensebox_write_sdcard_display_clear_test.xml",
+                config);
+    }
+
+    @Test
     public void buzzerLedRgbTest() throws Exception {
         Configuration config = this.senseboxHelper.regenerateConfiguration(Util1.readResourceContent("/ast/actions/sensebox_buzzer_led_rgb_test_config.xml"));
         this.senseboxHelper
