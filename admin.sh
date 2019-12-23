9a12492f9cac9d73d0d9b3c70815fb3937f05c98
Ramanjs <raman.s2662@gmail.com>
2019-12-23 22:00:07 +0530

    issue #353 fixed typo in admin.sh

--- admin.sh
+++ admin.sh
@@ -26,7 +26,7 @@ do
                    shift; shift ;;
     -Xmx*)         XMX=$1
                    shift ;;
-    -rdg)          RDBG='-agentlib:jdwp=transport=dt_socket,server=y,address=8000,suspend=y'
+    -rdbg)          RDBG='-agentlib:jdwp=transport=dt_socket,server=y,address=8000,suspend=y'
                    shift ;;
     -q)            QUIET='yes'
                    shift ;;
