ffa3227cdf1a513f6d9db1ba5c56d559e6145857
Caffetaria <ariavikram@gmail.com>
2020-01-04 18:47:24 +0530

    issue #478 Add unsupported warning to sound sensor in ev3 simulation

--- RobotEV3/src/main/java/de/fhg/iais/roberta/visitor/validate/Ev3SimValidatorVisitor.java
+++ RobotEV3/src/main/java/de/fhg/iais/roberta/visitor/validate/Ev3SimValidatorVisitor.java
@@ -5,6 +5,7 @@ import de.fhg.iais.roberta.components.ConfigurationAst;
 import de.fhg.iais.roberta.syntax.action.ev3.ShowPictureAction;
 import de.fhg.iais.roberta.syntax.sensor.generic.CompassSensor;
 import de.fhg.iais.roberta.syntax.sensor.generic.IRSeekerSensor;
+import de.fhg.iais.roberta.syntax.sensor.generic.SoundSensor;
 import de.fhg.iais.roberta.typecheck.NepoInfo;
 import de.fhg.iais.roberta.visitor.hardware.IEv3Visitor;
 
@@ -24,6 +25,12 @@ public final class Ev3SimValidatorVisitor extends AbstractSimValidatorVisitor im
     public Void visitCompassSensor(CompassSensor<Void> compassSensor) {
         super.visitCompassSensor(compassSensor);
         compassSensor.addInfo(NepoInfo.warning("SIM_BLOCK_NOT_SUPPORTED"));
+        return null;
+    }
+    
+    @Override
+    public Void visitSoundSensor(SoundSensor<Void> soundSensor) {
+        soundSensor.addInfo(NepoInfo.warning("SIM_BLOCK_NOT_SUPPORTED"));
         return null;
     }
 
