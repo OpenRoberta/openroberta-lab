97f0d11f229666dbf2ff09b88b57b16a30e3d583
Timo Fischer <timo.fischer1308@gmail.com>
2020-12-30 15:04:13 +0100

    added alignment integration to the frontend
    Disabled the color sensor if the alignment is set to "HORIZONTAL"

--- OpenRobertaServer/staticResources/js/app/simulation/simulationLogic/robot.nxt.js
+++ OpenRobertaServer/staticResources/js/app/simulation/simulationLogic/robot.nxt.js
@@ -10,8 +10,8 @@ define(['simulation.simulation', 'interpreter.constants', 'simulation.robot.ev3'
      * 
      * @class
      */
-    function Nxt(pose, configuration, positionConfiguration, num, robotBehaviour) {
-        Ev3.call(this, pose, configuration, positionConfiguration, num, robotBehaviour);
+    function Nxt(pose, configuration, sensorSettings, num, robotBehaviour) {
+        Ev3.call(this, pose, configuration, sensorSettings, num, robotBehaviour);
 
         this.geom = {
             x: -20,
