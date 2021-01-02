c029c1df4567e8a5e688a8f70c3ff83019a82135
Timo Fischer <timo.fischer1308@gmail.com>
2021-01-02 15:24:38 +0100

    Revert "Merge branch 'feature/color-sensor-alignment' into feature/color-sensor-position" KORREKTUR der falschen Branch
    This reverts commit d3444fe56480da38d5ae3f185b521becd1874d71, reversing
    changes made to 4aa67730569dadcacd1df1347fe723885273b1a3.

--- OpenRobertaRobot/src/main/java/de/fhg/iais/roberta/components/ConfigurationComponent.java
+++ OpenRobertaRobot/src/main/java/de/fhg/iais/roberta/components/ConfigurationComponent.java
@@ -134,15 +134,6 @@ public class ConfigurationComponent extends Phrase<Void> {
         }
     }
 
-    public String getColourAlignment(String block) {
-        switch ( getProperty(block) ) {
-            case SC.HORIZONTAL:
-                return SC.HORIZONTAL;
-            default:
-                return SC.DOWN;
-        }
-    }
-
     public String getInternalPortName() {
         return this.internalPortName;
     }
