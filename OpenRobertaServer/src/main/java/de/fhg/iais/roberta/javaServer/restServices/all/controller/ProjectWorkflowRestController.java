80a111f2fe68591258971ddd28176a0bc8d8aaa4
Timo Fischer <timo.fischer1308@gmail.com>
2021-01-02 15:24:38 +0100

    Revert "Merge branch 'feature/color-sensor-alignment' into feature/color-sensor-position" KORREKTUR der falschen Branch
    This reverts commit d3444fe56480da38d5ae3f185b521becd1874d71, reversing
    changes made to 4aa67730569dadcacd1df1347fe723885273b1a3.

--- OpenRobertaServer/src/main/java/de/fhg/iais/roberta/javaServer/restServices/all/controller/ProjectWorkflowRestController.java
+++ OpenRobertaServer/src/main/java/de/fhg/iais/roberta/javaServer/restServices/all/controller/ProjectWorkflowRestController.java
@@ -93,7 +93,6 @@ public class ProjectWorkflowRestController {
             response.setConfAnnos(project.getConfAnnotationList());
             response.setJavaScriptConfiguration(project.getSimSensorConfigurationJSON());
             response.setJavaScriptPositionConfiguration(project.getSimSensorPositionConfigurationJSON());
-            response.setJavaScriptAlignmentConfiguration(project.getSimSensorAlignmentConfigurationJSON());
             addProjectResultToResponse(response, project);
             Statistics.info("SimulationRun", "LoggedIn", httpSessionState.isUserLoggedIn(), "success", project.hasSucceeded());
             return UtilForREST.responseWithFrontendInfo(response, httpSessionState, this.robotCommunicator);
