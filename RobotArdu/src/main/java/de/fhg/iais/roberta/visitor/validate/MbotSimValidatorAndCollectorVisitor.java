fa2613c62201a5503be472cabd25cb79504d6327
Adrian Grzegorczyk <adrian.grzegorczyk@iais.fraunhofer.de>
2022-04-07 13:30:34 +0200

    Issue #1259: refactor mBot robot

--- RobotArdu/src/main/java/de/fhg/iais/roberta/visitor/validate/MbotSimValidatorAndCollectorVisitor.java
+++ RobotArdu/src/main/java/de/fhg/iais/roberta/visitor/validate/MbotSimValidatorAndCollectorVisitor.java
@@ -5,24 +5,16 @@ import com.google.common.collect.ClassToInstanceMap;
 import de.fhg.iais.roberta.bean.IProjectBean;
 import de.fhg.iais.roberta.components.ConfigurationAst;
 import de.fhg.iais.roberta.components.ConfigurationComponent;
-import de.fhg.iais.roberta.syntax.Phrase;
-import de.fhg.iais.roberta.syntax.SC;
-import de.fhg.iais.roberta.syntax.actors.arduino.LEDMatrixImageAction;
-import de.fhg.iais.roberta.syntax.actors.arduino.LEDMatrixSetBrightnessAction;
-import de.fhg.iais.roberta.syntax.actors.arduino.LEDMatrixTextAction;
 import de.fhg.iais.roberta.syntax.actors.arduino.mbot.ReceiveIRAction;
 import de.fhg.iais.roberta.syntax.actors.arduino.mbot.SendIRAction;
-import de.fhg.iais.roberta.syntax.expressions.arduino.LEDMatrixImage;
-import de.fhg.iais.roberta.syntax.functions.arduino.LEDMatrixImageInvertFunction;
-import de.fhg.iais.roberta.syntax.functions.arduino.LEDMatrixImageShiftFunction;
 import de.fhg.iais.roberta.syntax.sensor.ExternalSensor;
 import de.fhg.iais.roberta.syntax.sensor.generic.IRSeekerSensor;
 import de.fhg.iais.roberta.syntax.sensor.generic.LightSensor;
 import de.fhg.iais.roberta.typecheck.NepoInfo;
 import de.fhg.iais.roberta.visitor.hardware.IMbotVisitor;
 
-public class MbotSimValidatorVisitor extends AbstractSimValidatorVisitor implements IMbotVisitor<Void> {
-    public MbotSimValidatorVisitor(ConfigurationAst brickConfiguration, ClassToInstanceMap<IProjectBean.IBuilder<?>> beanBuilders) {
+public class MbotSimValidatorAndCollectorVisitor extends MbotValidatorAndCollectorVisitor implements IMbotVisitor<Void> {
+    public MbotSimValidatorAndCollectorVisitor(ConfigurationAst brickConfiguration, ClassToInstanceMap<IProjectBean.IBuilder<?>> beanBuilders) {
         super(brickConfiguration, beanBuilders);
     }
 
@@ -48,28 +40,6 @@ public class MbotSimValidatorVisitor extends AbstractSimValidatorVisitor impleme
     public Void visitReceiveIRAction(ReceiveIRAction<Void> receiveIRAction) {
         receiveIRAction.addInfo(NepoInfo.warning("SIM_BLOCK_NOT_SUPPORTED"));
         return null;
-    }
-
-    @Override
-    protected void checkDiffDrive(Phrase<Void> driveAction) {
-        checkLeftRightMotorPortMbed(driveAction);
-    }
-
-    private void checkLeftRightMotorPortMbed(Phrase<Void> driveAction) {
-        if ( validNumberOfMotors(driveAction) ) {
-            ConfigurationComponent leftMotor = this.robotConfiguration.getFirstMotor(SC.LEFT);
-            ConfigurationComponent rightMotor = this.robotConfiguration.getFirstMotor(SC.RIGHT);
-            if ( leftMotor == null ) {
-                driveAction.addInfo(NepoInfo.error("CONFIGURATION_ERROR_MOTOR_LEFT_MISSING"));
-                this.errorCount++;
-
-                if ( rightMotor == null ) {
-                    driveAction.addInfo(NepoInfo.error("CONFIGURATION_ERROR_MOTOR_RIGHT_MISSING"));
-                    this.errorCount++;
-
-                }
-            }
-        }
     }
 
     @Override
@@ -125,34 +95,4 @@ public class MbotSimValidatorVisitor extends AbstractSimValidatorVisitor impleme
         }
     }
 
-    //TODO Implement validation for LED Matrix
-    @Override
-    public Void visitLEDMatrixImageAction(LEDMatrixImageAction<Void> ledMatrixImageAction) {
-        return null;
-    }
-
-    @Override
-    public Void visitLEDMatrixTextAction(LEDMatrixTextAction<Void> ledMatrixTextAction) {
-        return null;
-    }
-
-    @Override
-    public Void visitLEDMatrixImage(LEDMatrixImage<Void> ledMatrixImage) {
-        return null;
-    }
-
-    @Override
-    public Void visitLEDMatrixImageShiftFunction(LEDMatrixImageShiftFunction<Void> ledMatrixImageShiftFunction) {
-        return null;
-    }
-
-    @Override
-    public Void visitLEDMatrixImageInvertFunction(LEDMatrixImageInvertFunction<Void> ledMatrixImageInverFunction) {
-        return null;
-    }
-
-    @Override
-    public Void visitLEDMatrixSetBrightnessAction(LEDMatrixSetBrightnessAction<Void> ledMatrixSetBrightnessAction) {
-        return null;
-    }
 }
---
+++ RobotArdu/src/main/java/de/fhg/iais/roberta/visitor/validate/MbotValidatorAndCollectorVisitor.java
@@ -0,0 +1,188 @@
+package de.fhg.iais.roberta.visitor.validate;
+
+import java.util.Objects;
+import java.util.Optional;
+
+import com.google.common.collect.ClassToInstanceMap;
+
+import de.fhg.iais.roberta.bean.IProjectBean;
+import de.fhg.iais.roberta.components.ConfigurationAst;
+import de.fhg.iais.roberta.components.ConfigurationComponent;
+import de.fhg.iais.roberta.components.UsedActor;
+import de.fhg.iais.roberta.syntax.MotionParam;
+import de.fhg.iais.roberta.syntax.MotorDuration;
+import de.fhg.iais.roberta.syntax.Phrase;
+import de.fhg.iais.roberta.syntax.SC;
+import de.fhg.iais.roberta.syntax.action.Action;
+import de.fhg.iais.roberta.syntax.action.motor.MotorGetPowerAction;
+import de.fhg.iais.roberta.syntax.action.motor.MotorSetPowerAction;
+import de.fhg.iais.roberta.syntax.action.motor.MotorStopAction;
+import de.fhg.iais.roberta.syntax.action.motor.differential.CurveAction;
+import de.fhg.iais.roberta.syntax.action.motor.differential.DriveAction;
+import de.fhg.iais.roberta.syntax.action.motor.differential.MotorDriveStopAction;
+import de.fhg.iais.roberta.syntax.action.motor.differential.TurnAction;
+import de.fhg.iais.roberta.syntax.action.sound.PlayNoteAction;
+import de.fhg.iais.roberta.syntax.action.sound.ToneAction;
+import de.fhg.iais.roberta.syntax.actors.arduino.LEDMatrixImageAction;
+import de.fhg.iais.roberta.syntax.actors.arduino.LEDMatrixSetBrightnessAction;
+import de.fhg.iais.roberta.syntax.actors.arduino.LEDMatrixTextAction;
+import de.fhg.iais.roberta.syntax.actors.arduino.mbot.ReceiveIRAction;
+import de.fhg.iais.roberta.syntax.actors.arduino.mbot.SendIRAction;
+import de.fhg.iais.roberta.syntax.expressions.arduino.LEDMatrixImage;
+import de.fhg.iais.roberta.syntax.functions.arduino.LEDMatrixImageInvertFunction;
+import de.fhg.iais.roberta.syntax.functions.arduino.LEDMatrixImageShiftFunction;
+import de.fhg.iais.roberta.syntax.lang.expr.Expr;
+import de.fhg.iais.roberta.syntax.lang.expr.NumConst;
+import de.fhg.iais.roberta.visitor.hardware.IMbotVisitor;
+
+public class MbotValidatorAndCollectorVisitor extends ArduinoValidatorAndCollectorVisitor implements IMbotVisitor<Void> {
+
+    public static final double DOUBLE_EPS = 1E-7;
+
+    public MbotValidatorAndCollectorVisitor(ConfigurationAst brickConfiguration, ClassToInstanceMap<IProjectBean.IBuilder<?>> beanBuilders) {
+        super(brickConfiguration, beanBuilders);
+    }
+
+
+    //TODO: To use usedMethodBuilder ast class is needed?
+    @Override
+    public Void visitDriveAction(DriveAction<Void> driveAction) {
+        checkAndVisitMotionParam(driveAction, driveAction.getParam());
+        checkLeftRightMotorPort(driveAction);
+        return null;
+    }
+
+    @Override
+    public Void visitTurnAction(TurnAction<Void> turnAction) {
+        checkAndVisitMotionParam(turnAction, turnAction.getParam());
+        checkLeftRightMotorPort(turnAction);
+        return null;
+    }
+
+    @Override
+    public Void visitCurveAction(CurveAction<Void> curveAction) {
+        requiredComponentVisited(curveAction, curveAction.getParamLeft().getSpeed(), curveAction.getParamRight().getSpeed());
+        Optional.ofNullable(curveAction.getParamLeft().getDuration()).ifPresent(duration -> requiredComponentVisited(curveAction, duration.getValue()));
+        Optional.ofNullable(curveAction.getParamRight().getDuration()).ifPresent(duration -> requiredComponentVisited(curveAction, duration.getValue()));
+        checkForZeroSpeedInCurve(curveAction.getParamLeft().getSpeed(), curveAction.getParamRight().getSpeed(), curveAction);
+        checkLeftRightMotorPort(curveAction);
+        return null;
+    }
+
+    protected void checkAndVisitMotionParam(Action<Void> action, MotionParam<Void> param) {
+        MotorDuration<Void> duration = param.getDuration();
+        Expr<Void> speed = param.getSpeed();
+        requiredComponentVisited(action, speed);
+        if ( duration != null ) {
+            requiredComponentVisited(action, duration.getValue());
+        }
+    }
+
+    private void checkLeftRightMotorPort(Phrase<Void> driveAction) {
+        ConfigurationComponent leftMotor = this.robotConfiguration.getFirstMotor(SC.LEFT);
+        ConfigurationComponent rightMotor = this.robotConfiguration.getFirstMotor(SC.RIGHT);
+        checkRightMotorPresenceAndLeftMotorPresence(driveAction, rightMotor, leftMotor);
+    }
+
+    private void checkForZeroSpeedInCurve(Expr<Void> speedLeft, Expr<Void> speedRight, Action<Void> action) {
+        if ( speedLeft.getKind().hasName("NUM_CONST") && speedRight.getKind().hasName("NUM_CONST") ) {
+            double speedLeftNumConst = Double.parseDouble(((NumConst<Void>) speedLeft).getValue());
+            double speedRightNumConst = Double.parseDouble(((NumConst<Void>) speedRight).getValue());
+            boolean bothMotorsHaveZeroSpeed = (Math.abs(speedLeftNumConst) < DOUBLE_EPS) && (Math.abs(speedRightNumConst) < DOUBLE_EPS);
+            if ( bothMotorsHaveZeroSpeed ) {
+                addWarningToPhrase(action, "MOTOR_SPEED_0");
+            }
+        }
+    }
+
+    private void checkRightMotorPresenceAndLeftMotorPresence(Phrase<Void> driveAction, ConfigurationComponent rightMotor, ConfigurationComponent leftMotor) {
+        if ( rightMotor == null ) {
+            addErrorToPhrase(driveAction, "CONFIGURATION_ERROR_MOTOR_RIGHT_MISSING");
+        }
+        if ( leftMotor == null ) {
+            addErrorToPhrase(driveAction, "CONFIGURATION_ERROR_MOTOR_LEFT_MISSING");
+        }
+    }
+
+
+    @Override
+    public Void visitMotorDriveStopAction(MotorDriveStopAction<Void> stopAction) {
+        return null;
+    }
+
+    @Override
+    public Void visitMotorGetPowerAction(MotorGetPowerAction<Void> motorGetPowerAction) {
+        addWarningToPhrase(motorGetPowerAction, "BLOCK_NOT_SUPPORTED");
+        return null;
+    }
+
+    @Override
+    public Void visitMotorSetPowerAction(MotorSetPowerAction<Void> motorSetPowerAction) {
+        return null;
+    }
+
+    @Override
+    public Void visitMotorStopAction(MotorStopAction<Void> motorStopAction) {
+        return null;
+    }
+
+    @Override
+    public Void visitSendIRAction(SendIRAction<Void> sendIRAction) {
+        requiredComponentVisited(sendIRAction, sendIRAction.getMessage());
+        return null;
+    }
+
+    @Override
+    public Void visitReceiveIRAction(ReceiveIRAction<Void> receiveIRAction) {
+        return null;
+    }
+
+    @Override
+    public Void visitLEDMatrixImageAction(LEDMatrixImageAction<Void> ledMatrixImageAction) {
+        requiredComponentVisited(ledMatrixImageAction, ledMatrixImageAction.getValuesToDisplay());
+        return null;
+    }
+
+    @Override
+    public Void visitLEDMatrixTextAction(LEDMatrixTextAction<Void> ledMatrixTextAction) {
+        requiredComponentVisited(ledMatrixTextAction, ledMatrixTextAction.getMsg());
+        return null;
+    }
+
+    @Override
+    public Void visitLEDMatrixImage(LEDMatrixImage<Void> ledMatrixImage) {
+        return null;
+    }
+
+    @Override
+    public Void visitLEDMatrixImageShiftFunction(LEDMatrixImageShiftFunction<Void> ledMatrixImageShiftFunction) {
+        requiredComponentVisited(ledMatrixImageShiftFunction, ledMatrixImageShiftFunction.getImage());
+        return null;
+    }
+
+    @Override
+    public Void visitLEDMatrixImageInvertFunction(LEDMatrixImageInvertFunction<Void> ledMatrixImageInverFunction) {
+        requiredComponentVisited(ledMatrixImageInverFunction, ledMatrixImageInverFunction.getImage());
+        return null;
+    }
+
+    @Override
+    public Void visitLEDMatrixSetBrightnessAction(LEDMatrixSetBrightnessAction<Void> ledMatrixSetBrightnessAction) {
+        requiredComponentVisited(ledMatrixSetBrightnessAction, ledMatrixSetBrightnessAction.getBrightness());
+        return null;
+    }
+
+    //TODO: Need mBot a Buzzer?
+    @Override
+    public Void visitPlayNoteAction(PlayNoteAction<Void> playNoteAction) {
+        return null;
+    }
+
+    @Override
+    public Void visitToneAction(ToneAction<Void> toneAction) {
+        requiredComponentVisited(toneAction, toneAction.getDuration(), toneAction.getFrequency());
+        return null;
+    }
+
+
+}
