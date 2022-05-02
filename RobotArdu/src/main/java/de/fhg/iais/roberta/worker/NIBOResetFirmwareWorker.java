0409a3c80893903a039abdc962e6f7f3d6a8abd8
Raghuvir Vinayak Shirodkar <raghuvir.vinayak.shirodkar@iais.fraunhofer.de>
2022-05-02 11:23:44 +0200

    Issue #1254: add (forgotten) support for reset with OpenRobertaConnector program.

--- RobotArdu/src/main/java/de/fhg/iais/roberta/worker/NIBOResetFirmwareWorker.java
+++ RobotArdu/src/main/java/de/fhg/iais/roberta/worker/NIBOResetFirmwareWorker.java
@@ -1,5 +1,9 @@
 package de.fhg.iais.roberta.worker;
 
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -20,11 +24,14 @@ public class NIBOResetFirmwareWorker implements IWorker {
 
         Key resultKey;
         String path = properties.getCompilerResourceDir() + "/" + project.getCompiledProgramPath() + "." + project.getBinaryFileExtension();
+        final File source = new File(path);
+        final File dest = new File(properties.getTempDir() + project.getToken() + "/" + project.getProgramName() + "/target");
 
         try {
             project.setCompiledHex(Util.getBase64EncodedHex(path, NIBOHexPrefix.getHexPrefixForRobot(project.getRobot())));
+            FileUtils.copyFileToDirectory(source, dest);
             resultKey = Key.FIRMWARE_RESET_SUCCESS;
-        } catch ( IllegalArgumentException e ) {
+        } catch ( IllegalArgumentException | IOException e ) {
             LOG.error("Reading default firmware for NIBO robots failed", e);
             resultKey = Key.FIRMWARE_RESET_ERROR;
         }
