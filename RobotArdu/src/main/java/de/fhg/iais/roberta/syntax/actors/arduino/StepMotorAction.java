e79c9ff89cbe24b515b70cab68823ce6bd41bab1
Reinhard Budde <reinhard.budde@iais.fraunhofer.de>
2022-07-01 13:21:12 +0200

    Issue #1319: small corrections in 2 classes

--- RobotArdu/src/main/java/de/fhg/iais/roberta/syntax/actors/arduino/StepMotorAction.java
+++ RobotArdu/src/main/java/de/fhg/iais/roberta/syntax/actors/arduino/StepMotorAction.java
@@ -3,12 +3,14 @@ package de.fhg.iais.roberta.syntax.actors.arduino;
 import de.fhg.iais.roberta.syntax.action.Action;
 import de.fhg.iais.roberta.syntax.lang.expr.Expr;
 import de.fhg.iais.roberta.transformer.forClass.NepoPhrase;
+import de.fhg.iais.roberta.transformer.forField.NepoValue;
 import de.fhg.iais.roberta.util.ast.BlocklyBlockProperties;
 import de.fhg.iais.roberta.util.ast.BlocklyComment;
 import de.fhg.iais.roberta.util.dbc.Assert;
 
 @NepoPhrase(name = "FESTOBIONIC_STEPMOTOR", category = "ACTOR", blocklyNames = {"festobionicActions_stepmotor"})
 public final class StepMotorAction<V> extends Action<V> {
+    @NepoValue(name = "VALUE")
     public final Expr<V> stepMotorPos;
 
     public StepMotorAction(BlocklyBlockProperties properties, BlocklyComment comment, Expr<V> stepMotorPos) {
--- RobotNAO/src/main/java/de/fhg/iais/roberta/syntax/action/nao/PlayFile.java
+++ RobotNAO/src/main/java/de/fhg/iais/roberta/syntax/action/nao/PlayFile.java
@@ -3,14 +3,14 @@ package de.fhg.iais.roberta.syntax.action.nao;
 import de.fhg.iais.roberta.syntax.action.Action;
 import de.fhg.iais.roberta.syntax.lang.expr.Expr;
 import de.fhg.iais.roberta.transformer.forClass.NepoPhrase;
-import de.fhg.iais.roberta.transformer.forField.NepoField;
+import de.fhg.iais.roberta.transformer.forField.NepoValue;
 import de.fhg.iais.roberta.util.ast.BlocklyBlockProperties;
 import de.fhg.iais.roberta.util.ast.BlocklyComment;
 import de.fhg.iais.roberta.util.dbc.Assert;
 
 @NepoPhrase(name = "PLAY_FILE", category = "ACTOR", blocklyNames = {"naoActions_playFile"})
 public final class PlayFile<V> extends Action<V> {
-    @NepoField(name = "OUT")
+    @NepoValue(name = "OUT")
     public final Expr<V> msg;
 
     public PlayFile(BlocklyBlockProperties properties, BlocklyComment comment, Expr<V> msg) {
