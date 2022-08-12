d7689ba7bb69f11632a88715596194a242d3078e
Beate Jost <beate.jost@iais.fraunhofer.de>
2022-08-12 06:48:41 +0200

    issue #1341: fix gallery layout

--- OpenRobertaServer/staticResources/js/app/roberta/controller/galleryList.controller.js
+++ OpenRobertaServer/staticResources/js/app/roberta/controller/galleryList.controller.js
@@ -1,21 +1,7 @@
 define(["require", "exports", "util", "message", "guiState.controller", "progList.model", "program.model", "program.controller", "blockly", "jquery", "bootstrap-table", "bootstrap-tagsinput"], function (require, exports, UTIL, MSG, GUISTATE_C, PROGLIST, PROGRAM, PROGRAM_C, Blockly, $) {
     Object.defineProperty(exports, "__esModule", { value: true });
     exports.formatTags = exports.formatDate = exports.formatAuthor = exports.formatProgramDescription = exports.formatProgramName = exports.formatRobot = exports.titleLikes = exports.titleNumberOfViews = exports.rowAttributes = exports.rowStyle = exports.init = void 0;
-    var BACKGROUND_COLORS = [
-        '#33B8CA',
-        '#EBC300',
-        '#39378B',
-        '#005A94',
-        '#179C7D',
-        '#F29400',
-        '#E2001A',
-        '#EB6A0A',
-        '#8FA402',
-        '#BACC1E',
-        '#9085BA',
-        '#FF69B4',
-        '#DF01D7',
-    ];
+    var BACKGROUND_COLORS = ['#33B8CA', '#EBC300', '#005A94', '#179C7D', '#F29400', '#E2001A', '#EB6A0A', '#8FA402', '#BACC1E', '#9085BA', '#FF69B4', '#DF01D7'];
     var currentColorIndex;
     var currentViewMode = 'gallery';
     /**
@@ -208,6 +194,7 @@ define(["require", "exports", "util", "message", "guiState.controller", "progLis
             field: 8,
             value: like,
         });
+        configureTagsInput();
     }
     var eventsLike = {
         'click .like': function (e, value, row, index) {
