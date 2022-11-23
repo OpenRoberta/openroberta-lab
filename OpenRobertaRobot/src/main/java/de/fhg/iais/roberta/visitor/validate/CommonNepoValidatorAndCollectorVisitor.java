6a38bf15ce051175ad4791b5fb8ef5279f0363ca
Patric Mellinghaus <mellinghauspatric@gmail.com>
2022-11-23 19:26:53 +0100

    adds error message to assignStmt and var blocks

--- OpenRobertaRobot/src/main/java/de/fhg/iais/roberta/visitor/validate/CommonNepoValidatorAndCollectorVisitor.java
+++ OpenRobertaRobot/src/main/java/de/fhg/iais/roberta/visitor/validate/CommonNepoValidatorAndCollectorVisitor.java
@@ -104,7 +104,7 @@ public abstract class CommonNepoValidatorAndCollectorVisitor extends AbstractVal
             this.getBuilder(UsedHardwareBean.Builder.class).addMarkedVariableAsGlobal(variableName);
         }
         if ( !builder.containsInScopeVariable(variableName) ) {
-            addErrorToPhrase(assignStmt, "");
+            addErrorToPhrase(assignStmt, "PROCEDURES_VARIABLES_OUT_OF_SCOPE_ERROR");
         }
         return null;
     }
@@ -553,7 +553,7 @@ public abstract class CommonNepoValidatorAndCollectorVisitor extends AbstractVal
     public Void visitVar(Var var) {
         String variableName = var.name;
         if ( !(this.getBuilder(UsedHardwareBean.Builder.class).containsInScopeVariable(variableName)) ) {
-            addErrorToPhrase(var, "");
+            addErrorToPhrase(var, "PROCEDURES_VARIABLES_OUT_OF_SCOPE_ERROR");
         }
         return null;
     }
