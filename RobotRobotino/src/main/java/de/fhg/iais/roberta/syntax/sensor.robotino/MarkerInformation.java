640e5088ea08e3ab3fc6c2f48973bebcdbee17a9
heini208 <mstemmeler@gmx.de>
2022-12-14 15:25:54 +0100

    Fix missing XML-Fields in AST

--- RobotRobotino/src/main/java/de/fhg/iais/roberta/syntax/sensor.robotino/MarkerInformation.java
+++ RobotRobotino/src/main/java/de/fhg/iais/roberta/syntax/sensor.robotino/MarkerInformation.java
@@ -1,9 +1,11 @@
 package de.fhg.iais.roberta.syntax.sensor.robotino;
 
+import de.fhg.iais.roberta.blockly.generated.Hide;
 import de.fhg.iais.roberta.syntax.lang.expr.Expr;
 import de.fhg.iais.roberta.syntax.sensor.Sensor;
 import de.fhg.iais.roberta.transformer.forClass.NepoExpr;
 import de.fhg.iais.roberta.transformer.forField.NepoField;
+import de.fhg.iais.roberta.transformer.forField.NepoHide;
 import de.fhg.iais.roberta.transformer.forField.NepoValue;
 import de.fhg.iais.roberta.typecheck.BlocklyType;
 import de.fhg.iais.roberta.util.ast.BlocklyProperties;
@@ -21,12 +23,17 @@ public final class MarkerInformation extends Sensor implements WithUserDefinedPo
 
     @NepoValue(name = "VALUE", type = BlocklyType.NUMBER)
     public final Expr markerId;
+    
+    @NepoHide
+    public final Hide hide;
 
-    public MarkerInformation(BlocklyProperties properties, String mode, String port, Expr markerId) {
+
+    public MarkerInformation(BlocklyProperties properties, String mode, String port, Expr markerId, Hide hide) {
         super(properties);
         this.mode = mode;
         this.port = port;
         this.markerId = markerId;
+        this.hide = hide;
         setReadOnly();
     }
 
