623a418d9cd415c49f3463781c9b14ee463f0254
heini208 <mstemmeler@gmx.de>
2023-01-13 14:05:00 +0100

    Issue #1442: Add Light sensor to quadRGBBlock using grayscale method

--- RobotCyberpi/src/main/java/de/fhg/iais/roberta/syntax/sensor/mbot2/QuadRGBSensor.java
+++ RobotCyberpi/src/main/java/de/fhg/iais/roberta/syntax/sensor/mbot2/QuadRGBSensor.java
@@ -7,7 +7,7 @@ import de.fhg.iais.roberta.util.ast.BlocklyProperties;
 import de.fhg.iais.roberta.util.ast.ExternalSensorBean;
 
 @NepoExpr(name = "QUAD_COLOR_SENSING", category = "SENSOR", blocklyNames = {"robSensors_line_getSample", "robSensors_quadrgb_getSample"},
-    sampleValues = {@F2M(field = "QUADRGB_COLOUR", mode = "COLOUR"), @F2M(field = "QUADRGB_AMBIENTLIGHT", mode = "AMBIENTLIGHT"), @F2M(field = "QUADRGB_LINE", mode = "LINE")})
+    sampleValues = {@F2M(field = "QUADRGB_COLOUR", mode = "COLOUR"), @F2M(field = "QUADRGB_LIGHT", mode = "LIGHT"), @F2M(field = "QUADRGB_AMBIENTLIGHT", mode = "AMBIENTLIGHT"), @F2M(field = "QUADRGB_LINE", mode = "LINE")})
 public final class QuadRGBSensor extends ExternalSensor {
 
     public QuadRGBSensor(BlocklyProperties properties, ExternalSensorBean externalSensorBean) {
