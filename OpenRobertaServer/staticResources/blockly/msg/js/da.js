heini208/roberta

Add code generation for Aifes in NANO33BLE-plugin

OpenRobertaServer/staticResources/blockly/msg/js/da.js
@@ -10,6 +10,7 @@ Blockly.Msg.ABOUT = "about";  // untranslated
 Blockly.Msg.ACCELERATION_TOOLTIP = "Get the acceleration value in milli-gravities.";  // untranslated
 Blockly.Msg.ACCELEROMETER_ROTATION_TOOLTIP = "Get the tilt or rotations in degrees.";  // untranslated
 Blockly.Msg.ACCELEROMETER_TOOLTIP = "Represents an accelerometer.";  // untranslated
+Blockly.Msg.ACTION_AIFES = "Aifes";  // untranslated
 Blockly.Msg.ACTION_ANALOGIN = "actuator analog";  // untranslated
 Blockly.Msg.ACTION_BUZZER = "buzzer";  // untranslated
 Blockly.Msg.ACTION_BUZZER_ARDUINO = "buzzer HYT120";  // untranslated
@@ -54,7 +55,8 @@ Blockly.Msg.ACTOR_DIGITALIN_TOOLTIP = "Writes a HIGH or a LOW value to a digital
 Blockly.Msg.ACTOR_TOOLTIP = "Repræsenterer en aktør.";
 Blockly.Msg.ADDRESS = "address";  // untranslated
 Blockly.Msg.ADD_COMMENT = "Tilføj kommentar";
-Blockly.Msg.AIFES_ADD_RAW_DATA = "add raw data for later feature extraction";  // untranslated
+Blockly.Msg.AIFES = "Aifes";  // untranslated
+Blockly.Msg.AIFES_ADD_RAW_DATA = "add to dataset";  // untranslated
 Blockly.Msg.AIFES_ADD_RAW_DATA_TOOLTIP = "add raw data (multiple times). Then extract the input features from it and add that to the trainings data";  // untranslated
 Blockly.Msg.AIFES_ADD_TRAININGS_DATA = "add to trainings data";  // untranslated
 Blockly.Msg.AIFES_ADD_TRAININGS_DATA_TOOLTIP = "take raw data, execute feature extraction and add the data from feature extraction to the collection of training data for a class";  // untranslated
@@ -62,16 +64,29 @@ Blockly.Msg.AIFES_CLASSIFY = "classifiy";  // untranslated
 Blockly.Msg.AIFES_CLASSIFY_TOOLTIP = "use the trained neural network and data from feature extraction to classify and return the probabiliy for each class";  // untranslated
 Blockly.Msg.AIFES_CLASS_NUMBER = "class #";  // untranslated
 Blockly.Msg.AIFES_CLASS_PROBABILITIES = "probabilities";  // untranslated
-Blockly.Msg.AIFES_INIT_RAW_DATA = "initialize raw data";  // untranslated
+Blockly.Msg.AIFES_DATASET = "Dataset";  // untranslated
+Blockly.Msg.AIFES_EPOCHS = "Epochs";  // untranslated
+Blockly.Msg.AIFES_FNN_LAYERS = "# layers";  // untranslated
+Blockly.Msg.AIFES_INIT_RAW_DATA = "initialize dataset";  // untranslated
 Blockly.Msg.AIFES_INIT_RAW_DATA_TOOLTIP = "clear raw data memory. Then add raw data multiple times, extract the input features from it and add that to the trainings data";  // untranslated
+Blockly.Msg.AIFES_LEARNINGFUNCTION = "Learning function";  // untranslated
+Blockly.Msg.AIFES_LEARNINGRATE = "Learning rate";  // untranslated
+Blockly.Msg.AIFES_LOSS = "Loss";  // untranslated
 Blockly.Msg.AIFES_MAX_NUMBER_OF_NEURONS = "max neurons";  // untranslated
+Blockly.Msg.AIFES_MAX_WEIGHT = "max weight";  // untranslated
+Blockly.Msg.AIFES_MIN_WEIGHT = "min weight";  // untranslated
+Blockly.Msg.AIFES_MOMENTUM = "Momentum";  // untranslated
+Blockly.Msg.AIFES_NUMBER_HIDDENLAYERS_NEURONS = "# hidden layers";  // untranslated
 Blockly.Msg.AIFES_NUMBER_INPUT_NEURONS = "# input neurons";  // untranslated
 Blockly.Msg.AIFES_NUMBER_OF_CLASSES = "# classes";  // untranslated
-Blockly.Msg.AIFES_RAW_DATA = "raw value";  // untranslated
+Blockly.Msg.AIFES_NUMBER_OUTPUT_NEURONS = "# output neurons";  // untranslated
+Blockly.Msg.AIFES_OPTIMIER = "Optimizer";  // untranslated
+Blockly.Msg.AIFES_RAW_DATA = "data";  // untranslated
 Blockly.Msg.AIFES_SETUP = "setup the neural network";  // untranslated
 Blockly.Msg.AIFES_SETUP_TOOLTIP = "define properties of a neural network, which can classify data";  // untranslated
 Blockly.Msg.AIFES_TRAIN = "train";  // untranslated
 Blockly.Msg.AIFES_TRAIN_TOOLTIP = "train the neural network with the trainings data assembled";  // untranslated
+Blockly.Msg.AIFES_WEIGHT = "weight";  // untranslated
 Blockly.Msg.ALL_RGBLED = "RGB LED all";  // untranslated
 Blockly.Msg.ANALOG = "analog";  // untranslated
 Blockly.Msg.ANALOGIN_TOOLTIP = "Represents any actuator connected to an analog pin.";  // untranslated

OpenRobertaServer/staticResources/blockly/msg/js/ms.js
@@ -10,6 +10,7 @@ Blockly.Msg.ABOUT = "about";  // untranslated
 Blockly.Msg.ACCELERATION_TOOLTIP = "Get the acceleration value in milli-gravities.";  // untranslated
 Blockly.Msg.ACCELEROMETER_ROTATION_TOOLTIP = "Get the tilt or rotations in degrees.";  // untranslated
 Blockly.Msg.ACCELEROMETER_TOOLTIP = "Represents an accelerometer.";  // untranslated
+Blockly.Msg.ACTION_AIFES = "Aifes";  // untranslated
 Blockly.Msg.ACTION_ANALOGIN = "actuator analog";  // untranslated
 Blockly.Msg.ACTION_BUZZER = "buzzer";  // untranslated
 Blockly.Msg.ACTION_BUZZER_ARDUINO = "buzzer HYT120";  // untranslated
@@ -54,7 +55,8 @@ Blockly.Msg.ACTOR_DIGITALIN_TOOLTIP = "Writes a HIGH or a LOW value to a digital
 Blockly.Msg.ACTOR_TOOLTIP = "Represents any actor.";  // untranslated
 Blockly.Msg.ADDRESS = "address";  // untranslated
 Blockly.Msg.ADD_COMMENT = "Berikan Komen";
-Blockly.Msg.AIFES_ADD_RAW_DATA = "add raw data for later feature extraction";  // untranslated
+Blockly.Msg.AIFES = "Aifes";  // untranslated
+Blockly.Msg.AIFES_ADD_RAW_DATA = "add to dataset";  // untranslated
 Blockly.Msg.AIFES_ADD_RAW_DATA_TOOLTIP = "add raw data (multiple times). Then extract the input features from it and add that to the trainings data";  // untranslated
 Blockly.Msg.AIFES_ADD_TRAININGS_DATA = "add to trainings data";  // untranslated
 Blockly.Msg.AIFES_ADD_TRAININGS_DATA_TOOLTIP = "take raw data, execute feature extraction and add the data from feature extraction to the collection of training data for a class";  // untranslated
@@ -62,16 +64,29 @@ Blockly.Msg.AIFES_CLASSIFY = "classifiy";  // untranslated
 Blockly.Msg.AIFES_CLASSIFY_TOOLTIP = "use the trained neural network and data from feature extraction to classify and return the probabiliy for each class";  // untranslated
 Blockly.Msg.AIFES_CLASS_NUMBER = "class #";  // untranslated
 Blockly.Msg.AIFES_CLASS_PROBABILITIES = "probabilities";  // untranslated
-Blockly.Msg.AIFES_INIT_RAW_DATA = "initialize raw data";  // untranslated
+Blockly.Msg.AIFES_DATASET = "Dataset";  // untranslated
+Blockly.Msg.AIFES_EPOCHS = "Epochs";  // untranslated
+Blockly.Msg.AIFES_FNN_LAYERS = "# layers";  // untranslated
+Blockly.Msg.AIFES_INIT_RAW_DATA = "initialize dataset";  // untranslated
 Blockly.Msg.AIFES_INIT_RAW_DATA_TOOLTIP = "clear raw data memory. Then add raw data multiple times, extract the input features from it and add that to the trainings data";  // untranslated
+Blockly.Msg.AIFES_LEARNINGFUNCTION = "Learning function";  // untranslated
+Blockly.Msg.AIFES_LEARNINGRATE = "Learning rate";  // untranslated
+Blockly.Msg.AIFES_LOSS = "Loss";  // untranslated
 Blockly.Msg.AIFES_MAX_NUMBER_OF_NEURONS = "max neurons";  // untranslated
+Blockly.Msg.AIFES_MAX_WEIGHT = "max weight";  // untranslated
+Blockly.Msg.AIFES_MIN_WEIGHT = "min weight";  // untranslated
+Blockly.Msg.AIFES_MOMENTUM = "Momentum";  // untranslated
+Blockly.Msg.AIFES_NUMBER_HIDDENLAYERS_NEURONS = "# hidden layers";  // untranslated
 Blockly.Msg.AIFES_NUMBER_INPUT_NEURONS = "# input neurons";  // untranslated
 Blockly.Msg.AIFES_NUMBER_OF_CLASSES = "# classes";  // untranslated
-Blockly.Msg.AIFES_RAW_DATA = "raw value";  // untranslated
+Blockly.Msg.AIFES_NUMBER_OUTPUT_NEURONS = "# output neurons";  // untranslated
+Blockly.Msg.AIFES_OPTIMIER = "Optimizer";  // untranslated
+Blockly.Msg.AIFES_RAW_DATA = "data";  // untranslated
 Blockly.Msg.AIFES_SETUP = "setup the neural network";  // untranslated
 Blockly.Msg.AIFES_SETUP_TOOLTIP = "define properties of a neural network, which can classify data";  // untranslated
 Blockly.Msg.AIFES_TRAIN = "train";  // untranslated
 Blockly.Msg.AIFES_TRAIN_TOOLTIP = "train the neural network with the trainings data assembled";  // untranslated
+Blockly.Msg.AIFES_WEIGHT = "weight";  // untranslated
 Blockly.Msg.ALL_RGBLED = "RGB LED all";  // untranslated
 Blockly.Msg.ANALOG = "analog";  // untranslated
 Blockly.Msg.ANALOGIN_TOOLTIP = "Represents any actuator connected to an analog pin.";  // untranslated

OpenRobertaServer/staticResources/blockly/msg/js/uk.js
@@ -10,6 +10,7 @@ Blockly.Msg.ABOUT = "про";
 Blockly.Msg.ACCELERATION_TOOLTIP = "Отримати прискорення у галах";
 Blockly.Msg.ACCELEROMETER_ROTATION_TOOLTIP = "Get the tilt or rotations in degrees.";  // untranslated
 Blockly.Msg.ACCELEROMETER_TOOLTIP = "Represents an accelerometer.";  // untranslated
+Blockly.Msg.ACTION_AIFES = "Aifes";  // untranslated
 Blockly.Msg.ACTION_ANALOGIN = "actuator analog";  // untranslated
 Blockly.Msg.ACTION_BUZZER = "зумер";
 Blockly.Msg.ACTION_BUZZER_ARDUINO = "buzzer HYT120";  // untranslated
@@ -54,7 +55,8 @@ Blockly.Msg.ACTOR_DIGITALIN_TOOLTIP = "Writes a HIGH or a LOW value to a digital
 Blockly.Msg.ACTOR_TOOLTIP = "Представляє будь-яку активну деталь.";
 Blockly.Msg.ADDRESS = "address";  // untranslated
 Blockly.Msg.ADD_COMMENT = "Додати коментар";
-Blockly.Msg.AIFES_ADD_RAW_DATA = "add raw data for later feature extraction";  // untranslated
+Blockly.Msg.AIFES = "Aifes";  // untranslated
+Blockly.Msg.AIFES_ADD_RAW_DATA = "add to dataset";  // untranslated
 Blockly.Msg.AIFES_ADD_RAW_DATA_TOOLTIP = "add raw data (multiple times). Then extract the input features from it and add that to the trainings data";  // untranslated
 Blockly.Msg.AIFES_ADD_TRAININGS_DATA = "add to trainings data";  // untranslated
 Blockly.Msg.AIFES_ADD_TRAININGS_DATA_TOOLTIP = "take raw data, execute feature extraction and add the data from feature extraction to the collection of training data for a class";  // untranslated
@@ -62,16 +64,29 @@ Blockly.Msg.AIFES_CLASSIFY = "classifiy";  // untranslated
 Blockly.Msg.AIFES_CLASSIFY_TOOLTIP = "use the trained neural network and data from feature extraction to classify and return the probabiliy for each class";  // untranslated
 Blockly.Msg.AIFES_CLASS_NUMBER = "class #";  // untranslated
 Blockly.Msg.AIFES_CLASS_PROBABILITIES = "probabilities";  // untranslated
-Blockly.Msg.AIFES_INIT_RAW_DATA = "initialize raw data";  // untranslated
+Blockly.Msg.AIFES_DATASET = "Dataset";  // untranslated
+Blockly.Msg.AIFES_EPOCHS = "Epochs";  // untranslated
+Blockly.Msg.AIFES_FNN_LAYERS = "# layers";  // untranslated
+Blockly.Msg.AIFES_INIT_RAW_DATA = "initialize dataset";  // untranslated
 Blockly.Msg.AIFES_INIT_RAW_DATA_TOOLTIP = "clear raw data memory. Then add raw data multiple times, extract the input features from it and add that to the trainings data";  // untranslated
+Blockly.Msg.AIFES_LEARNINGFUNCTION = "Learning function";  // untranslated
+Blockly.Msg.AIFES_LEARNINGRATE = "Learning rate";  // untranslated
+Blockly.Msg.AIFES_LOSS = "Loss";  // untranslated
 Blockly.Msg.AIFES_MAX_NUMBER_OF_NEURONS = "max neurons";  // untranslated
+Blockly.Msg.AIFES_MAX_WEIGHT = "max weight";  // untranslated
+Blockly.Msg.AIFES_MIN_WEIGHT = "min weight";  // untranslated
+Blockly.Msg.AIFES_MOMENTUM = "Momentum";  // untranslated
+Blockly.Msg.AIFES_NUMBER_HIDDENLAYERS_NEURONS = "# hidden layers";  // untranslated
 Blockly.Msg.AIFES_NUMBER_INPUT_NEURONS = "# input neurons";  // untranslated
 Blockly.Msg.AIFES_NUMBER_OF_CLASSES = "# classes";  // untranslated
-Blockly.Msg.AIFES_RAW_DATA = "raw value";  // untranslated
+Blockly.Msg.AIFES_NUMBER_OUTPUT_NEURONS = "# output neurons";  // untranslated
+Blockly.Msg.AIFES_OPTIMIER = "Optimizer";  // untranslated
+Blockly.Msg.AIFES_RAW_DATA = "data";  // untranslated
 Blockly.Msg.AIFES_SETUP = "setup the neural network";  // untranslated
 Blockly.Msg.AIFES_SETUP_TOOLTIP = "define properties of a neural network, which can classify data";  // untranslated
 Blockly.Msg.AIFES_TRAIN = "train";  // untranslated
 Blockly.Msg.AIFES_TRAIN_TOOLTIP = "train the neural network with the trainings data assembled";  // untranslated
+Blockly.Msg.AIFES_WEIGHT = "weight";  // untranslated
 Blockly.Msg.ALL_RGBLED = "RGB LED all";  // untranslated
 Blockly.Msg.ANALOG = "аналог";
 Blockly.Msg.ANALOGIN_TOOLTIP = "Represents any actuator connected to an analog pin.";  // untranslated

OpenRoberta/openroberta-lab

	Adding declarations and parameters into setup-loop
	Implementing Action Blocks for Aifes
	fix add to Output
	Adjustment of Classify-Block
	Add comma-operator for Classify-Block

OpenRobertaServer/staticResources/blockly/msg/js/da.js
@@ -10,6 +10,7 @@ Blockly.Msg.ABOUT = "about";  // untranslated
 Blockly.Msg.ACCELERATION_TOOLTIP = "Get the acceleration value in milli-gravities.";  // untranslated
 Blockly.Msg.ACCELEROMETER_ROTATION_TOOLTIP = "Get the tilt or rotations in degrees.";  // untranslated
 Blockly.Msg.ACCELEROMETER_TOOLTIP = "Represents an accelerometer.";  // untranslated
+Blockly.Msg.ACTION_AIFES = "Aifes";  // untranslated
 Blockly.Msg.ACTION_ANALOGIN = "actuator analog";  // untranslated
 Blockly.Msg.ACTION_BUZZER = "buzzer";  // untranslated
 Blockly.Msg.ACTION_BUZZER_ARDUINO = "buzzer HYT120";  // untranslated
@@ -56,24 +57,42 @@ Blockly.Msg.ACTOR_DIGITALIN_TOOLTIP = "Writes a HIGH or a LOW value to a digital
 Blockly.Msg.ACTOR_TOOLTIP = "Repræsenterer en aktør.";
 Blockly.Msg.ADDRESS = "address";  // untranslated
 Blockly.Msg.ADD_COMMENT = "Tilføj kommentar";
-Blockly.Msg.AIFES_ADD_RAW_DATA = "add raw data for later feature extraction";  // untranslated
-Blockly.Msg.AIFES_ADD_RAW_DATA_TOOLTIP = "add raw data (multiple times). Then extract the input features from it and add that to the trainings data";  // untranslated
-Blockly.Msg.AIFES_ADD_TRAININGS_DATA = "add to trainings data";  // untranslated
-Blockly.Msg.AIFES_ADD_TRAININGS_DATA_TOOLTIP = "take raw data, execute feature extraction and add the data from feature extraction to the collection of training data for a class";  // untranslated
-Blockly.Msg.AIFES_CLASSIFY = "classifiy";  // untranslated
+Blockly.Msg.AIFES = "Aifes";  // untranslated
+Blockly.Msg.AIFES_ADD_CLASSIFY_DATA = "add classify data";  // untranslated
+Blockly.Msg.AIFES_ADD_CLASSIFY_DATA_TOOLTIP = "add classify data (multiple times). Then extract the input features from it and add that to the classifiy data";  // untranslated
+Blockly.Msg.AIFES_ADD_RAW_DATA = "add to training dataset";  // untranslated
+Blockly.Msg.AIFES_ADD_RAW_DATA_TOOLTIP = "add training data (multiple times). Then extract the input features from it and add that to the trainings data";  // untranslated
+Blockly.Msg.AIFES_ADD_TARGET_DATA = "add to target data";  // untranslated
+Blockly.Msg.AIFES_ADD_TARGET_DATA_TOOLTIP = "take raw data, execute feature extraction and add the data from feature extraction to the collection of training data for a class";  // untranslated
+Blockly.Msg.AIFES_CLASSIFY = "classify";  // untranslated
 Blockly.Msg.AIFES_CLASSIFY_TOOLTIP = "use the trained neural network and data from feature extraction to classify and return the probabiliy for each class";  // untranslated
-Blockly.Msg.AIFES_CLASS_NUMBER = "class #";  // untranslated
+Blockly.Msg.AIFES_CLASS_NUMBER = "class number";  // untranslated
 Blockly.Msg.AIFES_CLASS_PROBABILITIES = "probabilities";  // untranslated
-Blockly.Msg.AIFES_INIT_RAW_DATA = "initialize raw data";  // untranslated
+Blockly.Msg.AIFES_DATASET = "dataset";  // untranslated
+Blockly.Msg.AIFES_EPOCHS = "epochs";  // untranslated
+Blockly.Msg.AIFES_FNN_LAYERS = "number layers";  // untranslated
+Blockly.Msg.AIFES_INIT_CLASSIFY_DATA = "initialize classify dataset";  // untranslated
+Blockly.Msg.AIFES_INIT_CLASSIFY_DATA_TOOLTIP = "clear classify data memory. Then add raw data multiple times, extract the input features from it and add that to the classify data";  // untranslated
+Blockly.Msg.AIFES_INIT_RAW_DATA = "initialize training dataset";  // untranslated
 Blockly.Msg.AIFES_INIT_RAW_DATA_TOOLTIP = "clear raw data memory. Then add raw data multiple times, extract the input features from it and add that to the trainings data";  // untranslated
+Blockly.Msg.AIFES_LEARNINGFUNCTION = "activations function";  // untranslated
+Blockly.Msg.AIFES_LEARNINGRATE = "learning rate";  // untranslated
+Blockly.Msg.AIFES_LOSS = "loss";  // untranslated
 Blockly.Msg.AIFES_MAX_NUMBER_OF_NEURONS = "max neurons";  // untranslated
-Blockly.Msg.AIFES_NUMBER_INPUT_NEURONS = "# input neurons";  // untranslated
-Blockly.Msg.AIFES_NUMBER_OF_CLASSES = "# classes";  // untranslated
-Blockly.Msg.AIFES_RAW_DATA = "raw value";  // untranslated
+Blockly.Msg.AIFES_MAX_WEIGHT = "max weight";  // untranslated
+Blockly.Msg.AIFES_MIN_WEIGHT = "min weight";  // untranslated
+Blockly.Msg.AIFES_MOMENTUM = "momentum";  // untranslated
+Blockly.Msg.AIFES_NUMBER_HIDDENLAYERS_NEURONS = "number hidden layers";  // untranslated
+Blockly.Msg.AIFES_NUMBER_INPUT_NEURONS = "number input neurons";  // untranslated
+Blockly.Msg.AIFES_NUMBER_OF_CLASSES = "number classes";  // untranslated
+Blockly.Msg.AIFES_NUMBER_OUTPUT_NEURONS = "number output neurons";  // untranslated
+Blockly.Msg.AIFES_OPTIMIER = "optimizer";  // untranslated
+Blockly.Msg.AIFES_RAW_DATA = "data";  // untranslated
 Blockly.Msg.AIFES_SETUP = "setup the neural network";  // untranslated
 Blockly.Msg.AIFES_SETUP_TOOLTIP = "define properties of a neural network, which can classify data";  // untranslated
 Blockly.Msg.AIFES_TRAIN = "train";  // untranslated
 Blockly.Msg.AIFES_TRAIN_TOOLTIP = "train the neural network with the trainings data assembled";  // untranslated
+Blockly.Msg.AIFES_WEIGHT = "weight";  // untranslated
 Blockly.Msg.ALL_RGBLED = "RGB LED all";  // untranslated
 Blockly.Msg.ANALOG = "analog";  // untranslated
 Blockly.Msg.ANALOGIN_TOOLTIP = "Represents any actuator connected to an analog pin.";  // untranslated

OpenRobertaServer/staticResources/blockly/msg/js/ms.js
@@ -10,6 +10,7 @@ Blockly.Msg.ABOUT = "about";  // untranslated
 Blockly.Msg.ACCELERATION_TOOLTIP = "Get the acceleration value in milli-gravities.";  // untranslated
 Blockly.Msg.ACCELEROMETER_ROTATION_TOOLTIP = "Get the tilt or rotations in degrees.";  // untranslated
 Blockly.Msg.ACCELEROMETER_TOOLTIP = "Represents an accelerometer.";  // untranslated
+Blockly.Msg.ACTION_AIFES = "Aifes";  // untranslated
 Blockly.Msg.ACTION_ANALOGIN = "actuator analog";  // untranslated
 Blockly.Msg.ACTION_BUZZER = "buzzer";  // untranslated
 Blockly.Msg.ACTION_BUZZER_ARDUINO = "buzzer HYT120";  // untranslated
@@ -56,24 +57,42 @@ Blockly.Msg.ACTOR_DIGITALIN_TOOLTIP = "Writes a HIGH or a LOW value to a digital
 Blockly.Msg.ACTOR_TOOLTIP = "Represents any actor.";  // untranslated
 Blockly.Msg.ADDRESS = "address";  // untranslated
 Blockly.Msg.ADD_COMMENT = "Berikan Komen";
-Blockly.Msg.AIFES_ADD_RAW_DATA = "add raw data for later feature extraction";  // untranslated
-Blockly.Msg.AIFES_ADD_RAW_DATA_TOOLTIP = "add raw data (multiple times). Then extract the input features from it and add that to the trainings data";  // untranslated
-Blockly.Msg.AIFES_ADD_TRAININGS_DATA = "add to trainings data";  // untranslated
-Blockly.Msg.AIFES_ADD_TRAININGS_DATA_TOOLTIP = "take raw data, execute feature extraction and add the data from feature extraction to the collection of training data for a class";  // untranslated
-Blockly.Msg.AIFES_CLASSIFY = "classifiy";  // untranslated
+Blockly.Msg.AIFES = "Aifes";  // untranslated
+Blockly.Msg.AIFES_ADD_CLASSIFY_DATA = "add classify data";  // untranslated
+Blockly.Msg.AIFES_ADD_CLASSIFY_DATA_TOOLTIP = "add classify data (multiple times). Then extract the input features from it and add that to the classifiy data";  // untranslated
+Blockly.Msg.AIFES_ADD_RAW_DATA = "add to training dataset";  // untranslated
+Blockly.Msg.AIFES_ADD_RAW_DATA_TOOLTIP = "add training data (multiple times). Then extract the input features from it and add that to the trainings data";  // untranslated
+Blockly.Msg.AIFES_ADD_TARGET_DATA = "add to target data";  // untranslated
+Blockly.Msg.AIFES_ADD_TARGET_DATA_TOOLTIP = "take raw data, execute feature extraction and add the data from feature extraction to the collection of training data for a class";  // untranslated
+Blockly.Msg.AIFES_CLASSIFY = "classify";  // untranslated
 Blockly.Msg.AIFES_CLASSIFY_TOOLTIP = "use the trained neural network and data from feature extraction to classify and return the probabiliy for each class";  // untranslated
-Blockly.Msg.AIFES_CLASS_NUMBER = "class #";  // untranslated
+Blockly.Msg.AIFES_CLASS_NUMBER = "class number";  // untranslated
 Blockly.Msg.AIFES_CLASS_PROBABILITIES = "probabilities";  // untranslated
-Blockly.Msg.AIFES_INIT_RAW_DATA = "initialize raw data";  // untranslated
+Blockly.Msg.AIFES_DATASET = "dataset";  // untranslated
+Blockly.Msg.AIFES_EPOCHS = "epochs";  // untranslated
+Blockly.Msg.AIFES_FNN_LAYERS = "number layers";  // untranslated
+Blockly.Msg.AIFES_INIT_CLASSIFY_DATA = "initialize classify dataset";  // untranslated
+Blockly.Msg.AIFES_INIT_CLASSIFY_DATA_TOOLTIP = "clear classify data memory. Then add raw data multiple times, extract the input features from it and add that to the classify data";  // untranslated
+Blockly.Msg.AIFES_INIT_RAW_DATA = "initialize training dataset";  // untranslated
 Blockly.Msg.AIFES_INIT_RAW_DATA_TOOLTIP = "clear raw data memory. Then add raw data multiple times, extract the input features from it and add that to the trainings data";  // untranslated
+Blockly.Msg.AIFES_LEARNINGFUNCTION = "activations function";  // untranslated
+Blockly.Msg.AIFES_LEARNINGRATE = "learning rate";  // untranslated
+Blockly.Msg.AIFES_LOSS = "loss";  // untranslated
 Blockly.Msg.AIFES_MAX_NUMBER_OF_NEURONS = "max neurons";  // untranslated
-Blockly.Msg.AIFES_NUMBER_INPUT_NEURONS = "# input neurons";  // untranslated
-Blockly.Msg.AIFES_NUMBER_OF_CLASSES = "# classes";  // untranslated
-Blockly.Msg.AIFES_RAW_DATA = "raw value";  // untranslated
+Blockly.Msg.AIFES_MAX_WEIGHT = "max weight";  // untranslated
+Blockly.Msg.AIFES_MIN_WEIGHT = "min weight";  // untranslated
+Blockly.Msg.AIFES_MOMENTUM = "momentum";  // untranslated
+Blockly.Msg.AIFES_NUMBER_HIDDENLAYERS_NEURONS = "number hidden layers";  // untranslated
+Blockly.Msg.AIFES_NUMBER_INPUT_NEURONS = "number input neurons";  // untranslated
+Blockly.Msg.AIFES_NUMBER_OF_CLASSES = "number classes";  // untranslated
+Blockly.Msg.AIFES_NUMBER_OUTPUT_NEURONS = "number output neurons";  // untranslated
+Blockly.Msg.AIFES_OPTIMIER = "optimizer";  // untranslated
+Blockly.Msg.AIFES_RAW_DATA = "data";  // untranslated
 Blockly.Msg.AIFES_SETUP = "setup the neural network";  // untranslated
 Blockly.Msg.AIFES_SETUP_TOOLTIP = "define properties of a neural network, which can classify data";  // untranslated
 Blockly.Msg.AIFES_TRAIN = "train";  // untranslated
 Blockly.Msg.AIFES_TRAIN_TOOLTIP = "train the neural network with the trainings data assembled";  // untranslated
+Blockly.Msg.AIFES_WEIGHT = "weight";  // untranslated
 Blockly.Msg.ALL_RGBLED = "RGB LED all";  // untranslated
 Blockly.Msg.ANALOG = "analog";  // untranslated
 Blockly.Msg.ANALOGIN_TOOLTIP = "Represents any actuator connected to an analog pin.";  // untranslated

OpenRobertaServer/staticResources/blockly/msg/js/uk.js
@@ -10,6 +10,7 @@ Blockly.Msg.ABOUT = "про";
 Blockly.Msg.ACCELERATION_TOOLTIP = "Отримати прискорення у галах";
 Blockly.Msg.ACCELEROMETER_ROTATION_TOOLTIP = "Get the tilt or rotations in degrees.";  // untranslated
 Blockly.Msg.ACCELEROMETER_TOOLTIP = "Represents an accelerometer.";  // untranslated
+Blockly.Msg.ACTION_AIFES = "Aifes";  // untranslated
 Blockly.Msg.ACTION_ANALOGIN = "actuator analog";  // untranslated
 Blockly.Msg.ACTION_BUZZER = "зумер";
 Blockly.Msg.ACTION_BUZZER_ARDUINO = "buzzer HYT120";  // untranslated
@@ -56,24 +57,42 @@ Blockly.Msg.ACTOR_DIGITALIN_TOOLTIP = "Writes a HIGH or a LOW value to a digital
 Blockly.Msg.ACTOR_TOOLTIP = "Представляє будь-яку активну деталь.";
 Blockly.Msg.ADDRESS = "address";  // untranslated
 Blockly.Msg.ADD_COMMENT = "Додати коментар";
-Blockly.Msg.AIFES_ADD_RAW_DATA = "add raw data for later feature extraction";  // untranslated
-Blockly.Msg.AIFES_ADD_RAW_DATA_TOOLTIP = "add raw data (multiple times). Then extract the input features from it and add that to the trainings data";  // untranslated
-Blockly.Msg.AIFES_ADD_TRAININGS_DATA = "add to trainings data";  // untranslated
-Blockly.Msg.AIFES_ADD_TRAININGS_DATA_TOOLTIP = "take raw data, execute feature extraction and add the data from feature extraction to the collection of training data for a class";  // untranslated
-Blockly.Msg.AIFES_CLASSIFY = "classifiy";  // untranslated
+Blockly.Msg.AIFES = "Aifes";  // untranslated
+Blockly.Msg.AIFES_ADD_CLASSIFY_DATA = "add classify data";  // untranslated
+Blockly.Msg.AIFES_ADD_CLASSIFY_DATA_TOOLTIP = "add classify data (multiple times). Then extract the input features from it and add that to the classifiy data";  // untranslated
+Blockly.Msg.AIFES_ADD_RAW_DATA = "add to training dataset";  // untranslated
+Blockly.Msg.AIFES_ADD_RAW_DATA_TOOLTIP = "add training data (multiple times). Then extract the input features from it and add that to the trainings data";  // untranslated
+Blockly.Msg.AIFES_ADD_TARGET_DATA = "add to target data";  // untranslated
+Blockly.Msg.AIFES_ADD_TARGET_DATA_TOOLTIP = "take raw data, execute feature extraction and add the data from feature extraction to the collection of training data for a class";  // untranslated
+Blockly.Msg.AIFES_CLASSIFY = "classify";  // untranslated
 Blockly.Msg.AIFES_CLASSIFY_TOOLTIP = "use the trained neural network and data from feature extraction to classify and return the probabiliy for each class";  // untranslated
-Blockly.Msg.AIFES_CLASS_NUMBER = "class #";  // untranslated
+Blockly.Msg.AIFES_CLASS_NUMBER = "class number";  // untranslated
 Blockly.Msg.AIFES_CLASS_PROBABILITIES = "probabilities";  // untranslated
-Blockly.Msg.AIFES_INIT_RAW_DATA = "initialize raw data";  // untranslated
+Blockly.Msg.AIFES_DATASET = "dataset";  // untranslated
+Blockly.Msg.AIFES_EPOCHS = "epochs";  // untranslated
+Blockly.Msg.AIFES_FNN_LAYERS = "number layers";  // untranslated
+Blockly.Msg.AIFES_INIT_CLASSIFY_DATA = "initialize classify dataset";  // untranslated
+Blockly.Msg.AIFES_INIT_CLASSIFY_DATA_TOOLTIP = "clear classify data memory. Then add raw data multiple times, extract the input features from it and add that to the classify data";  // untranslated
+Blockly.Msg.AIFES_INIT_RAW_DATA = "initialize training dataset";  // untranslated
 Blockly.Msg.AIFES_INIT_RAW_DATA_TOOLTIP = "clear raw data memory. Then add raw data multiple times, extract the input features from it and add that to the trainings data";  // untranslated
+Blockly.Msg.AIFES_LEARNINGFUNCTION = "activations function";  // untranslated
+Blockly.Msg.AIFES_LEARNINGRATE = "learning rate";  // untranslated
+Blockly.Msg.AIFES_LOSS = "loss";  // untranslated
 Blockly.Msg.AIFES_MAX_NUMBER_OF_NEURONS = "max neurons";  // untranslated
-Blockly.Msg.AIFES_NUMBER_INPUT_NEURONS = "# input neurons";  // untranslated
-Blockly.Msg.AIFES_NUMBER_OF_CLASSES = "# classes";  // untranslated
-Blockly.Msg.AIFES_RAW_DATA = "raw value";  // untranslated
+Blockly.Msg.AIFES_MAX_WEIGHT = "max weight";  // untranslated
+Blockly.Msg.AIFES_MIN_WEIGHT = "min weight";  // untranslated
+Blockly.Msg.AIFES_MOMENTUM = "momentum";  // untranslated
+Blockly.Msg.AIFES_NUMBER_HIDDENLAYERS_NEURONS = "number hidden layers";  // untranslated
+Blockly.Msg.AIFES_NUMBER_INPUT_NEURONS = "number input neurons";  // untranslated
+Blockly.Msg.AIFES_NUMBER_OF_CLASSES = "number classes";  // untranslated
+Blockly.Msg.AIFES_NUMBER_OUTPUT_NEURONS = "number output neurons";  // untranslated
+Blockly.Msg.AIFES_OPTIMIER = "optimizer";  // untranslated
+Blockly.Msg.AIFES_RAW_DATA = "data";  // untranslated
 Blockly.Msg.AIFES_SETUP = "setup the neural network";  // untranslated
 Blockly.Msg.AIFES_SETUP_TOOLTIP = "define properties of a neural network, which can classify data";  // untranslated
 Blockly.Msg.AIFES_TRAIN = "train";  // untranslated
 Blockly.Msg.AIFES_TRAIN_TOOLTIP = "train the neural network with the trainings data assembled";  // untranslated
+Blockly.Msg.AIFES_WEIGHT = "weight";  // untranslated
 Blockly.Msg.ALL_RGBLED = "RGB LED all";  // untranslated
 Blockly.Msg.ANALOG = "аналог";
 Blockly.Msg.ANALOGIN_TOOLTIP = "Represents any actuator connected to an analog pin.";  // untranslated