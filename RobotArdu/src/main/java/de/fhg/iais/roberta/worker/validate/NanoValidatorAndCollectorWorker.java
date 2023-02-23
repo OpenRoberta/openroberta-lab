443941e93c25f1837900bc433741b2bfa479bba2
Beate Jost <beate.jost@iais.fraunhofer.de>
2023-02-23 15:30:56 +0100

    Issue #1457: add A6 and A7 as valid Pins to Arduino Nano

--- RobotArdu/src/main/java/de/fhg/iais/roberta/worker/validate/NanoValidatorAndCollectorWorker.java
+++ RobotArdu/src/main/java/de/fhg/iais/roberta/worker/validate/NanoValidatorAndCollectorWorker.java
@@ -7,7 +7,7 @@ public class NanoValidatorAndCollectorWorker extends ArduinoValidatorAndCollecto
     public NanoValidatorAndCollectorWorker() {
         super(
             Stream
-                .of("LED_BUILTIN", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "A0", "A1", "A2", "A3", "A4", "A5")
+                .of("LED_BUILTIN", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "A0", "A1", "A2", "A3", "A4", "A5", "A6", "A7")
                 .collect(Collectors.toList()));
     }
 }
