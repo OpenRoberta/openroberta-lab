826c3888f87a6e802e25d7eada9818ad61f50b51
Patric Mellinghaus <mellinghauspatric@gmail.com>
2023-05-05 19:08:40 +0200

    Issue #1069: switching to configuration view updates the language of the blocks when changed

--- OpenRobertaWeb/src/app/roberta/controller/configuration.controller.js
+++ OpenRobertaWeb/src/app/roberta/controller/configuration.controller.js
@@ -369,13 +369,9 @@ function reloadConf(opt_result) {
 }
 
 function reloadView() {
-    if (isVisible()) {
-        var dom = confVis ? confVis.getXml() : Blockly.Xml.workspaceToDom(bricklyWorkspace);
-        var xml = Blockly.Xml.domToText(dom);
-        configurationToBricklyWorkspace(xml);
-    } else {
-        seen = false;
-    }
+    var dom = confVis ? confVis.getXml() : Blockly.Xml.workspaceToDom(bricklyWorkspace);
+    var xml = Blockly.Xml.domToText(dom);
+    configurationToBricklyWorkspace(xml);
     var toolbox = GUISTATE_C.getConfigurationToolbox();
     bricklyWorkspace.updateToolbox(toolbox);
 }
