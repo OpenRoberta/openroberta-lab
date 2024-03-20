84d6fe4331178d5577ce3dec8807a33d2ae83fd7
AyaMOussa <aya.alii@hotmail.com>
2024-03-20 21:05:14 +0100

    fix typo in default properties

--- RobotMbed/src/main/java/de/fhg/iais/roberta/worker/validate/CalliopeV3ValidatorAndCollectorWorker.java
+++ RobotMbed/src/main/java/de/fhg/iais/roberta/worker/validate/CalliopeV3ValidatorAndCollectorWorker.java
@@ -24,7 +24,7 @@ public class CalliopeV3ValidatorAndCollectorWorker extends MbedValidatorAndColle
      *     hashmap maps them to their specific pins, so they can be checked.
      */
     public static final List<String> FREE_PINS = Stream.of("0", "1", "2", "3", "4", "5", "C04", "C05", "C06", "C07", "C08", "C09", "C10", "C11", "C12", "C13", "C14", "C15", "C16", "C17", "C18", "C19", "C20", "M0", "M1").collect(Collectors.toList());
-    public static final List<String> DEFAULT_PROPERTIES = Stream.of("KEY", "RGBLEDs", "BUZZER", "LOGOTOUCH", "ACCELEROMETER", "GYRO", "COMPASS", "TEMPERATURE", "LIGHT", "SOUND", "ROBOT").collect(Collectors.toList());
+    public static final List<String> DEFAULT_PROPERTIES = Stream.of("KEY", "RGBLED", "BUZZER", "LOGOTOUCH", "ACCELEROMETER", "GYRO", "COMPASS", "TEMPERATURE", "LIGHT", "SOUND", "ROBOT").collect(Collectors.toList());
     public static final HashMap<String, String> MAP_CORRECT_CONFIG_PINS = new HashMap<String, String>() {{
         put(SC.ULTRASONIC, "5");
         put(SC.COLOUR, "4");
