9da5ca748988e7357ea4dc95e65dc405582d624b
AyaMOussa <aya.alii@hotmail.com>
2024-03-20 21:10:01 +0100

    create calliopev3 python generator

--- RobotMbed/src/main/java/de/fhg/iais/roberta/visitor/codegen/CalliopeV3PythonVisitor.java
+++ RobotMbed/src/main/java/de/fhg/iais/roberta/visitor/codegen/CalliopeV3PythonVisitor.java
@@ -7,8 +7,42 @@ import com.google.common.collect.ClassToInstanceMap;
 import de.fhg.iais.roberta.bean.IProjectBean;
 import de.fhg.iais.roberta.components.ConfigurationAst;
 import de.fhg.iais.roberta.syntax.Phrase;
+import de.fhg.iais.roberta.syntax.action.light.LedAction;
+import de.fhg.iais.roberta.syntax.action.light.RgbLedOffAction;
+import de.fhg.iais.roberta.syntax.action.light.RgbLedOffHiddenAction;
+import de.fhg.iais.roberta.syntax.action.light.RgbLedOnAction;
+import de.fhg.iais.roberta.syntax.action.light.RgbLedOnHiddenAction;
+import de.fhg.iais.roberta.syntax.action.mbed.BothMotorsOnAction;
+import de.fhg.iais.roberta.syntax.action.mbed.BothMotorsStopAction;
+import de.fhg.iais.roberta.syntax.action.mbed.DisplayGetBrightnessAction;
+import de.fhg.iais.roberta.syntax.action.mbed.DisplaySetBrightnessAction;
+import de.fhg.iais.roberta.syntax.action.mbed.FourDigitDisplayClearAction;
+import de.fhg.iais.roberta.syntax.action.mbed.FourDigitDisplayShowAction;
+import de.fhg.iais.roberta.syntax.action.mbed.LedBarSetAction;
+import de.fhg.iais.roberta.syntax.action.mbed.MotionKitDualSetAction;
+import de.fhg.iais.roberta.syntax.action.mbed.MotionKitSingleSetAction;
+import de.fhg.iais.roberta.syntax.action.mbed.ServoSetAction;
+import de.fhg.iais.roberta.syntax.action.mbed.SwitchLedMatrixAction;
+import de.fhg.iais.roberta.syntax.action.mbed.microbitV2.SoundToggleAction;
+import de.fhg.iais.roberta.syntax.action.motor.MotorGetPowerAction;
+import de.fhg.iais.roberta.syntax.action.motor.MotorOnAction;
+import de.fhg.iais.roberta.syntax.action.motor.MotorSetPowerAction;
+import de.fhg.iais.roberta.syntax.action.motor.MotorStopAction;
+import de.fhg.iais.roberta.syntax.action.sound.PlayFileAction;
+import de.fhg.iais.roberta.syntax.action.sound.SetVolumeAction;
+import de.fhg.iais.roberta.syntax.sensor.generic.ColorSensor;
+import de.fhg.iais.roberta.syntax.sensor.generic.GyroSensor;
+import de.fhg.iais.roberta.syntax.sensor.generic.HumiditySensor;
+import de.fhg.iais.roberta.syntax.sensor.generic.InfraredSensor;
+import de.fhg.iais.roberta.syntax.sensor.generic.SoundSensor;
+import de.fhg.iais.roberta.syntax.sensor.generic.UltrasonicSensor;
+import de.fhg.iais.roberta.syntax.sensor.mbed.RadioRssiSensor;
+import de.fhg.iais.roberta.syntax.sensor.mbed.microbitV2.LogoSetTouchMode;
+import de.fhg.iais.roberta.syntax.sensor.mbed.microbitV2.LogoTouchSensor;
+import de.fhg.iais.roberta.syntax.sensor.mbed.microbitV2.PinSetTouchMode;
+import de.fhg.iais.roberta.visitor.ICalliopeV3Visitor;
 
-public class CalliopeV3PythonVisitor extends MicrobitPythonVisitor {
+public class CalliopeV3PythonVisitor extends MbedPythonVisitor implements ICalliopeV3Visitor<Void> {
     /**
      * initialize the Python code generator visitor.
      *
@@ -21,5 +55,170 @@ public class CalliopeV3PythonVisitor extends MicrobitPythonVisitor {
         ConfigurationAst robotConfiguration,
         ClassToInstanceMap<IProjectBean> beans) {
         super(programPhrases, robotConfiguration, beans);
+    }
+
+    @Override
+    public Void visitMotorOnAction(MotorOnAction motorOnAction) {
+        return null;
+    }
+
+    @Override
+    public Void visitMotorSetPowerAction(MotorSetPowerAction motorSetPowerAction) {
+        return null;
+    }
+
+    @Override
+    public Void visitMotorGetPowerAction(MotorGetPowerAction motorGetPowerAction) {
+        return null;
+    }
+
+    @Override
+    public Void visitMotorStopAction(MotorStopAction motorStopAction) {
+        return null;
+    }
+
+    @Override
+    public Void visitSoundToggleAction(SoundToggleAction soundToggleAction) {
+        return null;
+    }
+
+    @Override
+    public Void visitSetVolumeAction(SetVolumeAction setVolumeAction) {
+        return null;
+    }
+
+    @Override
+    public Void visitSoundSensor(SoundSensor microphoneSensor) {
+        return null;
+    }
+
+    @Override
+    public Void visitLogoTouchSensor(LogoTouchSensor logoTouchSensor) {
+        return null;
+    }
+
+    @Override
+    public Void visitLogoSetTouchMode(LogoSetTouchMode logoSetTouchMode) {
+        return null;
+    }
+
+    @Override
+    public Void visitPinSetTouchMode(PinSetTouchMode pinSetTouchMode) {
+        return null;
+    }
+
+    @Override
+    public Void visitUltrasonicSensor(UltrasonicSensor ultrasonicSensor) {
+        return null;
+    }
+
+    @Override
+    public Void visitInfraredSensor(InfraredSensor infraredSensor) {
+        return null;
+    }
+
+    @Override
+    public Void visitGyroSensor(GyroSensor gyroSensor) {
+        return null;
+    }
+
+    @Override
+    public Void visitRgbLedOnAction(RgbLedOnAction rgbLedOnAction) {
+        return null;
+    }
+
+    @Override
+    public Void visitRgbLedOffAction(RgbLedOffAction rgbLedOffAction) {
+        return null;
+    }
+
+    @Override
+    public Void visitRgbLedOnHiddenAction(RgbLedOnHiddenAction rgbLedOnHiddenAction) {
+        return null;
+    }
+
+    @Override
+    public Void visitRgbLedOffHiddenAction(RgbLedOffHiddenAction rgbLedOffHiddenAction) {
+        return null;
+    }
+
+    @Override
+    public Void visitLedAction(LedAction ledAction) {
+        return null;
+    }
+
+    @Override
+    public Void visitRadioRssiSensor(RadioRssiSensor radioRssiSensor) {
+        return null;
+    }
+
+    @Override
+    public Void visitDisplaySetBrightnessAction(DisplaySetBrightnessAction displaySetBrightnessAction) {
+        return null;
+    }
+
+    @Override
+    public Void visitDisplayGetBrightnessAction(DisplayGetBrightnessAction displayGetBrightnessAction) {
+        return null;
+    }
+
+    @Override
+    public Void visitFourDigitDisplayShowAction(FourDigitDisplayShowAction fourDigitDisplayShowAction) {
+        return null;
+    }
+
+    @Override
+    public Void visitFourDigitDisplayClearAction(FourDigitDisplayClearAction fourDigitDisplayClearAction) {
+        return null;
+    }
+
+    @Override
+    public Void visitLedBarSetAction(LedBarSetAction ledBarSetAction) {
+        return null;
+    }
+
+    @Override
+    public Void visitBothMotorsOnAction(BothMotorsOnAction bothMotorsOnAction) {
+        return null;
+    }
+
+    @Override
+    public Void visitBothMotorsStopAction(BothMotorsStopAction bothMotorsStopAction) {
+        return null;
+    }
+
+    @Override
+    public Void visitHumiditySensor(HumiditySensor humiditySensor) {
+        return null;
+    }
+
+    @Override
+    public Void visitPlayFileAction(PlayFileAction playFileAction) {
+        return null;
+    }
+
+    @Override
+    public Void visitSwitchLedMatrixAction(SwitchLedMatrixAction switchLedMatrixAction) {
+        return null;
+    }
+
+    @Override
+    public Void visitServoSetAction(ServoSetAction servoSetAction) {
+        return null;
+    }
+
+    @Override
+    public Void visitMotionKitSingleSetAction(MotionKitSingleSetAction motionKitSingleSetAction) {
+        return null;
+    }
+
+    @Override
+    public Void visitMotionKitDualSetAction(MotionKitDualSetAction motionKitDualSetAction) {
+        return null;
+    }
+
+    @Override
+    public Void visitColorSensor(ColorSensor colorSensor) {
+        return null;
     }
 }
