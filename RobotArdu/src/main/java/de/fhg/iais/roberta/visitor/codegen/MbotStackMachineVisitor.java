6a042e9aeb6f25236f7e16fadc040639739171b7
AyaMOussa <aya.alii@hotmail.com>
2024-11-07 18:41:27 +0100

    Issue 1517: solve the uncaught pop error

--- RobotArdu/src/main/java/de/fhg/iais/roberta/visitor/codegen/MbotStackMachineVisitor.java
+++ RobotArdu/src/main/java/de/fhg/iais/roberta/visitor/codegen/MbotStackMachineVisitor.java
@@ -202,7 +202,7 @@ public class MbotStackMachineVisitor extends AbstractStackMachineVisitor impleme
         if ( speedOnly ) {
             return add(o.put(C.SET_TIME, false));
         } else {
-            add(o.put(C.SET_TIME, true));
+            o.put(C.SET_TIME, true);
             if ( duration.getType() == null ) {
                 o.put(C.MOTOR_DURATION, C.TIME);
 
