b67b39f9cc5d39675817a8d06638717591437298
AyaMOussa <aya.alii@hotmail.com>
2024-11-15 12:48:23 +0100

    Issue 1522: remove unnecessary semicolons

--- OpenRobertaServer/src/test/resources/crossCompilerTests/_expected/robotSpecific/targetLanguage/spikePybricks/actionSounds.py
+++ OpenRobertaServer/src/test/resources/crossCompilerTests/_expected/robotSpecific/targetLanguage/spikePybricks/actionSounds.py
@@ -20,11 +20,11 @@ ___C = float(261.6)
 def run():
     global ___A, ___C
     hub.display.icon(Matrix([[0, 0, 100, 0, 0], [0, 0, 100, 100, 0], [0, 0, 100, 0, 100], [100, 100, 100, 0, 0], [100, 100, 100, 0, 0]]))
-    hub.speaker.beep(261.626, 2000);
-    hub.speaker.beep(391.995, 1000);
-    hub.speaker.beep(783.991, 500);
-    hub.speaker.beep(329.628, 250);
-    hub.speaker.beep(146.832, 125);
+    hub.speaker.beep(261.626, 2000)
+    hub.speaker.beep(391.995, 1000)
+    hub.speaker.beep(783.991, 500)
+    hub.speaker.beep(329.628, 250)
+    hub.speaker.beep(146.832, 125)
     hub.speaker.beep(___A, 250)
     hub.speaker.beep(___C, 250)
 
